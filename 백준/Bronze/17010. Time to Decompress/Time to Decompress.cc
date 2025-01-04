c6031bc12b5a98b837405ba8c2f6c31fd43619c9
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-01-04 10:15:58 +0900

    [Bronze IV] Title: Time to Decompress, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+
+
+    while(t--) {
+        int n;
+        char c;
+        cin>>n>>c;
+
+        for(int i=0; i<n; i++) {
+            cout<<c;
+        }
+        cout<<"\n";
+    }
+}
