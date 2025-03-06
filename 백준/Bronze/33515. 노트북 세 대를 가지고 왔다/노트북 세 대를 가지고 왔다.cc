108cfd7e5d719223cde488b64e003755c5e80d77
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-03-06 11:14:39 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define MOD 1'000'000'007
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t1,t2;
+    cin>>t1>>t2;
+
+    if(t1>=t2) cout<<t2;
+    else {
+        cout<<t1;
+    }
+}
