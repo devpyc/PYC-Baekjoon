9757637d2d2f0bbfdb6b861aa5200c8566caab98
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-03-18 22:59:34 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    if(n==1) {
+        cout<<"2024 8";
+    }
+    if(n==2) {
+        cout<<"2025 3";
+    }
+    if(n==3) {
+        cout<<"2025 10";
+    }
+    if(n==4) {
+        cout<<"2026 5";
+    }
+    if(n==5) {
+        cout<<"2026 12";
+    }
+}
