3956bc1ba9ffce5cc1695f80ec5cd0340df23126
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-03-19 10:36:13 +0900

    [Silver IV] Title: 제로, Time: 24 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,41 +1,24 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define fi first
-#define se second
-#define pb push_back
-#define pf push_front
-#define pob pop_back
-#define pof pop_front
-#define pii pair<int,int>
-#define mi map<int,int>
-#define qi queue<int>
-#define di deque<int>
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define tiii tuple<int,int,int>
-#define MOD 9901
-#define endl "\n"
-#define io ios_base::sync_with_stdio(false); cin.tie(nullptr);
 using namespace std;
 
 int main() {
-    io;
-    int t;
-    cin >> t;
-    stack<int>n;
-    for(int i=0; i<t; ++i){
-        int N;
-        cin>>N;
-        if(N==0&&!n.empty()){
-            n.pop();
-        }else{
-            n.push(N);
+    int n;
+    cin>>n;
+
+    stack<int>st;
+    for(int i=0; i<n; i++) {
+        int x;
+        cin>>x;
+        if(x==0) {
+            st.pop();
+        }else {
+            st.push(x);
         }
     }
-    int s=0;
-    while(!n.empty()){
-        s+=n.top();
-        n.pop();
+    int ans=0;
+    while(!st.empty()) {
+        ans+=st.top();
+        st.pop();
     }
-    cout<<s<<endl;
+    cout<<ans;
 }
