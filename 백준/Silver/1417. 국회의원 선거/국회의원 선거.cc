099f7258166f7a1ef9edfcac5480c2e647632c86
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-13 18:03:08 +0900

    [Silver V] Title: 국회의원 선거, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.cc
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.cc
@@ -1,40 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int n;
-    cin >> n;
-
-    int a;
-    cin >> a;
-
-    priority_queue<int> pq;
-
-    for (int i = 1; i < n; i++) {
+    int n,a;
+    cin>>n>>a;
+    priority_queue<int>pq;
+    for (int i=1;i<n;i++) {
         int x;
-        cin >> x;
+        cin>>x;
         pq.push(x);
     }
-
-    int cnt = 0;
-
+    int cnt=0;
     if (pq.empty()) {
-        cout << 0 << "\n";
+        cout<<0;
         return 0;
     }
-
-    while (!pq.empty() && pq.top() >= a) {
-        int res = pq.top();
+    while (!pq.empty()&&pq.top()>=a) {
+        int res=pq.top();
         pq.pop();
-
         a++;
         res--;
         cnt++;
-
         pq.push(res);
     }
-    cout << cnt << "\n";
+    cout<<cnt;
 }
