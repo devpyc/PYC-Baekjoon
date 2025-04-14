327f49067d08173ec84b3b32d4817259a0562a21
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-14 15:44:10 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 256 ms, Memory: 5808 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -8,12 +8,11 @@ int main() {
     int n;
     cin>>n;
     int arr[n];
-    for(int i=0;i<n;i++) {
+    for(int i=0; i<n; i++) {
         cin>>arr[i];
     }
-
     sort(arr,arr+n,greater<int>());
-    for(int i=0;i<n;i++) {
-        cout<<arr[i]<<" ";
+    for(int i:arr) {
+        cout<<i<<"\n";
     }
 }
