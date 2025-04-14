939c6d4d864eeec89e1e41215517db6ca2935598
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-14 15:42:04 +0900

    [Silver V] Title: 생일, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.cc
+++ 백준/Silver/5635. 생일/생일.cc
@@ -5,28 +5,17 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    vector<pair<int,pair<int,pair<int,string>>>>arr;
     int n;
-    cin >> n;
+    cin>>n;
 
-    vector<tuple<string, int, int, int>> arr;
-
-    for(int i = 0; i < n; i++) {
-        string name;
-        int day, month, year;
-        cin >> name >> day >> month >> year;
-
-        arr.push_back(make_tuple(name, year, month, day));
+    for(int i=0; i<n; i++) {
+        string s;
+        int a,b,c;
+        cin>>s>>a>>b>>c;
+        arr.push_back({c,{b,{a,s}}});
     }
+    sort(arr.begin(),arr.end());
 
-    auto Max = *min_element(arr.begin(), arr.end(),
-        [](const auto& a, const auto& b) {
-            return make_tuple(get<1>(a), get<2>(a), get<3>(a)) < make_tuple(get<1>(b), get<2>(b), get<3>(b));
-        });
-
-    auto Min = *max_element(arr.begin(), arr.end(),
-        [](const auto& a, const auto& b) {
-            return make_tuple(get<1>(a), get<2>(a), get<3>(a)) < make_tuple(get<1>(b), get<2>(b), get<3>(b));
-        });
-
-    cout << get<0>(Min) << '\n' << get<0>(Max) << '\n';
+    cout<<arr[n-1].second.second.second<<"\n"<<arr[0].second.second.second;
 }
