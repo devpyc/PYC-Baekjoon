83fb1aba37919f9c376f14863a9f62aee0d9d5fe
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-15 18:35:20 +0900

    [Silver II] Title: 통계학, Time: 84 ms, Memory: 4252 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -1,80 +1,45 @@
 #include <bits/stdc++.h>
-#define int long long
-#define fi first
-#define se second
-#define pb push_back
-#define pi pair<int,int>
-#define mi map<int,int>
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define endl "\n"
-#define io ios_base::sync_with_stdio(false); cin.tie(nullptr);
 using namespace std;
 
-void build(vi& segTree, const vi& arr, int node, int start, int end) {
-    if (start == end) {
-        segTree[node] = arr[start];
-    } else {
-        int mid = (start + end) / 2;
-        build(segTree, arr, 2 * node + 1, start, mid);
-        build(segTree, arr, 2 * node + 2, mid + 1, end);
-        segTree[node] = segTree[2 * node + 1] + segTree[2 * node + 2];
-    }
-}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-int querySum(vi& segTree, int node, int start, int end, int L, int R) {
-    if (R < start || end < L) {
-        return 0;
-    }
-    if (L <= start && end <= R) {
-        return segTree[node];
-    }
-    int mid = (start + end) / 2;
-    int l = querySum(segTree, 2 * node + 1, start, mid, L, R);
-    int r = querySum(segTree, 2 * node + 2, mid + 1, end, L, R);
-    return l + r;
-}
+    int n;
+    cin>>n;
+    int arr[n];
+    double sum=0;
 
-int32_t main() {
-    io;
-    int N;
-    cin >> N;
-    vi arr(N);
-    mi frequency;
-    int sum = 0;
-
-    for (int i = 0; i < N; ++i) {
-        cin >> arr[i];
-        sum += arr[i];
-        frequency[arr[i]]++;
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+        sum+=arr[i];
     }
 
-    double mean = round((double)sum / N);
-    if (mean == -0) {
-        mean = 0;
+    int mean=round(sum/n);
+    sort(arr,arr+n);
+    map<int, int> freq;
+    for (int i : arr) {
+        freq[i]++;
     }
-    cout << mean << endl;
-
-    sort(arr.begin(), arr.end());
-    int median = arr[N / 2];
-    cout << median << endl;
-
-    int mode = arr[0];
-    int maxFreq = 0;
-    bool second = false;
-    for (auto& [num, freq] : frequency) {
-        if (freq > maxFreq) {
-            maxFreq = freq;
-            mode = num;
-            second = false;
-        } else if (freq == maxFreq && !second) {
-            mode = num;
-            second = true;
+    int fcnt=0;
+    for(auto pair:freq) {
+        fcnt=max(fcnt,pair.second);
+    }
+    vector<int> res;
+    for (auto pair : freq) {
+        if (pair.second == fcnt) {
+            res.push_back(pair.first);
         }
     }
-    cout << mode << endl;
 
-    int range = arr.back() - arr.front();
-    cout << range << endl;
-    return 0;
+    int cmd;
+    if(res.size()>1) {
+        cmd=res[1];
+    }else {
+        cmd=res[0];
+    }
+
+    int idx=arr[n-1]-arr[0];
+
+    cout<<mean<<"\n"<<arr[n/2]<<"\n"<<cmd<<"\n"<<idx;
 }
