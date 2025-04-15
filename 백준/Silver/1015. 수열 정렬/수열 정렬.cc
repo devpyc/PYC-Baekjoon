a7b2e0dc03d07468c3e59ad69dff06c3e786b42b
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-15 16:56:44 +0900

    [Silver IV] Title: 수열 정렬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1015. 수열 정렬/수열 정렬.cc
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.cc
@@ -1,41 +1,27 @@
 #include <bits/stdc++.h>
-#define endl "\n"
 using namespace std;
 
-struct Element {
-    int value;
-    int index;
-};
-
-bool compare(const Element& a, const Element& b) {
-    if (a.value == b.value) return a.index < b.index;
-    return a.value < b.value;
-}
-
 int main() {
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> A[i];
-    }
-    
-    vector<Element> elements(N);
-    for (int i = 0; i < N; ++i) {
-        elements[i] = {A[i], i};
-    }
-    
-    sort(elements.begin(), elements.end(), compare);
-    
-    vector<int> P(N);
-    for (int i = 0; i < N; ++i) {
-        P[elements[i].index] = i;
-    }
-    
-    for (int i = 0; i < N; ++i) {
-        cout << P[i] << " ";
-    }
-    cout << endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    return 0;
+    int n;
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    vector<int> b(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i].first;
+        a[i].second = i;
+    }
+
+    sort(a.begin(), a.end());
+
+    for(int i = 0; i < n; i++){
+        b[a[i].second] = i;
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << b[i] << " ";
+    }
 }
