2fd40e78ff61f9712f5e2c3bee1b19fd60202094
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-18 10:26:44 +0900

    [Silver V] Title: Passport Checking, Time: 68 ms, Memory: 5156 KB -BaekjoonHub

---
+++ 백준/Silver/13211. Passport Checking/Passport Checking.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+
+    vector<string>arr(n);
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+
+    int m;
+    cin>>m;
+
+    int cnt=0;
+    for(int i=0; i<m; i++) {
+        string s;
+        cin>>s;
+        if(binary_search(arr.begin(),arr.end(),s)) cnt++;
+    }
+    cout<<cnt;
+}
