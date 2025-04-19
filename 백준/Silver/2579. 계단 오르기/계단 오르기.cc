e1c76fdc782650a8fd4d50f5e00aca99a0d00cf1
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-19 18:16:58 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
-    vector<int> v(n + 1);
-    vector<int> dp(n + 1);
-    for (int i = 1; i <= n; i++) {
-        cin >> v[i];
+    vector<int> arr(n+1);
+    vector<int> dp(n+1);
+    for (int i = 1; i<n+1; i++) {
+        cin >> arr[i];
     }
-    dp[1] = v[1];
-    dp[2] = v[1] + v[2];
-    for (int i = 3; i <= n; i++) {
-        dp[i] = max(dp[i - 2] + v[i], dp[i - 3] + v[i - 1] + v[i]);
+    dp[1]=arr[1];
+    dp[2]=arr[1]+arr[2];
+    for (int i=3; i<n+1; i++) {
+        dp[i]=max(dp[i-2]+arr[i], dp[i-3]+arr[i-1] + arr[i]);
     }
     cout << dp[n];
 }
