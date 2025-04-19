52521139b2163919475e39e9c1da84dc24734871
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-19 10:24:50 +0900

    [Silver I] Title: 절댓값 힙, Time: 12 ms, Memory: 2532 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -1,37 +1,32 @@
-#include <iostream>
-#include <queue>
-#include <cmath>
-
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-struct Compare {
+struct cmp {
     bool operator()(int a, int b) {
-        if (abs(a) == abs(b))
-            return a > b;
-        return abs(a) > abs(b);
+        if(abs(a)==abs(b)) return a>b;
+        return abs(a)>abs(b);
     }
 };
 
 int main() {
-    int N;
-    cin >> N;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    priority_queue<int, vector<int>, Compare> pq;
-
-    while (N--) {
+    int n;
+    cin>>n;
+    priority_queue<int, vector<int>, cmp>pq;
+    for(int i=0; i<n; i++) {
         int x;
-        cin >> x;
-
-        if (x == 0) {
-            if (pq.empty()) {
-                cout << "0\n";
-            } else {
-                cout << pq.top() << "\n";
+        cin>>x;
+        if(x==0) {
+            if(pq.empty()) cout<<0<<"\n";
+            else {
+                cout<<pq.top()<<"\n";
                 pq.pop();
             }
-        } else {
+        }else {
             pq.push(x);
         }
     }
-    return 0;
 }
