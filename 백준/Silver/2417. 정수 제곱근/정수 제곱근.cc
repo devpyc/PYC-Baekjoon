e4f2418c4ba96098ea4eb51723bd35c79a1950ab
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-19 10:58:14 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -1,34 +1,17 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define endl "\n"
-#define io ios_base::sync_with_stdio(false);cin.tie(nullptr);
 using namespace std;
 
 int main() {
-    io;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     ll n;
-    cin >> n;
+    cin>>n;
 
-    if (n == 0) {
-        cout << 0 << endl;
-        return 0;
+    ll ans=sqrt(n);
+    if(ans*ans<n) {
+        ++ans;
     }
-
-    double x = n;
-    double prev = 0;
-
-    while (abs(x - prev) >= 1) {
-        prev = x;
-        x = (x + n / x) / 2.0;
-    }
-
-    ll result = static_cast<ll>(x);
-
-    if (result * result < n) {
-        result++;
-    }
-
-    cout << result << endl;
-
-    return 0;
+    cout<<ans;
 }
