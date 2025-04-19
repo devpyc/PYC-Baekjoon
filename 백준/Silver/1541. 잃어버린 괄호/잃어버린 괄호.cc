aa8d860b0c9db900ea49256a2ddb7918cc0011c1
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-19 18:05:21 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,34 +1,18 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s;
-    vector<int> num;
-    vector<char> op;
-    string temp;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '+' || s[i] == '-') {
-            num.push_back(stoi(temp));
-            temp = "";
-            op.push_back(s[i]);
-        } else {
-            temp += s[i];
-        }
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    char c;
+    int n,s,m=0;
+
+    for(scanf("%d",&s);scanf("%c",&c),c!=10;) {
+        if(c=='-')m=1;
+        scanf("%d",&n);
+        if(m)s-=n;
+        else s+=n;
     }
-    num.push_back(stoi(temp));
-    int ans = num[0];
-    bool minus = false;
-    for (int i = 0; i < op.size(); i++) {
-        if (op[i] == '-') {
-            minus = true;
-        }
-        if (minus) {
-            ans -= num[i + 1];
-        } else {
-            ans += num[i + 1];
-        }
-    }
-    cout << ans;
+    printf("%d",s);
 }
