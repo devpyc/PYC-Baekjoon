370529a4bce0bb947f51cba5bb5a9305dbc09e54
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2025-04-19 17:46:41 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,18 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define MOD 10007
 using namespace std;
 
-const int MOD = 10007;
-int dp[1001];
-
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
-    cin >> n;
+    cin>>n;
+    int dp[n+1];
 
-    dp[1] = 1;
-    dp[2] = 2;
-
-    for (int i = 3; i <= n; ++i) {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+    dp[1]=1,dp[2]=2;
+    for(int i=3; i<n+1; ++i) {
+        dp[i]=(dp[i-1]+dp[i-2])%MOD;
     }
-    cout << dp[n] << endl;
+    cout<<dp[n];
 }
