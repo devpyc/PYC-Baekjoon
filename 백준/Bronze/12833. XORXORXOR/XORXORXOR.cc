083566e3d9952839d299074981b5897289aec148
dpeyvc <136340169+dpeyvc@users.noreply.github.com>
2025-05-08 09:05:15 +0900

    [Bronze I] Title: XORXORXOR, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/12833. XORXORXOR/XORXORXOR.cc
+++ 백준/Bronze/12833. XORXORXOR/XORXORXOR.cc
@@ -4,12 +4,7 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    long long a,b,c;
+    int a,b,c;
     cin>>a>>b>>c;
-    if(c%2==1) {
-        cout<<(a^b);
-    }else {
-        cout<<a;
-    }
+    (c%2==1)?cout<<(a^b):cout<<a;
 }
