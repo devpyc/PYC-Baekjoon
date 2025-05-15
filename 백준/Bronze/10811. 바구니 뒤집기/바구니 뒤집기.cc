626110259d10a8665f7dbdd5d934fdc0ecd9e8cb
yechan <136340169+dpeyvc@users.noreply.github.com>
2025-05-15 12:44:37 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -1,31 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-    int N, M;
-    cin >> N >> M;
-
-    vector<int> basket(N);
-    for (int i = 0; i < N; i++) {
-        basket[i] = i + 1;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n,m;
+    cin>>n>>m;
+    int arr[n];
+    for(int i=1; i<=n; i++) {
+        arr[i-1]=i;
     }
-
-    for (int i = 0; i < M; i++) {
-        int start, end;
-        cin >> start >> end;
-        start--;
-        end--;
-
-        while (start < end) {
-            swap(basket[start], basket[end]);
-            start++;
-            end--;
-        }
+    while(m--) {
+        int a,b;
+        cin>>a>>b;
+        reverse(arr+(a-1),arr+b);
     }
-
-    for (int i = 0; i < N; i++) {
-        cout << basket[i] << " ";
+    for(int i:arr) {
+        cout<<i<<" ";
     }
-    cout << endl;
-
-    return 0;
 }
