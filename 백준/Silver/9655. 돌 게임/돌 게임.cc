5bb82e9efd6fde2b50d68d75baa0012ab9ad6efc
yechan <136340169+dpeyvc@users.noreply.github.com>
2025-05-24 21:08:23 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -1,15 +1,8 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int N;
-    cin>>N;
-    
-    if(N%2==1){
-        cout<<"SK"<<endl;
-    }else{
-        cout<<"CY"<<endl;
-    }
+int main() {
+    int n;
+    cin>>n;
+    (n%2==1)?cout<<"SK":cout<<"CY";
 }
