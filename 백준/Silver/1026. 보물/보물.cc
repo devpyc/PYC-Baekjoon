08e9005aec9cebd5211e8f3e2bda73fdc7bfafae
yechan <136340169+dpeyvc@users.noreply.github.com>
2025-05-24 19:54:02 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cc
+++ 백준/Silver/1026. 보물/보물.cc
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
-    cin >> n;
-    vector<int> a(n);
-    vector<int> b(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+    cin>>n;
+    int arr1[n],arr2[n];
+    for(int i=0; i<n; i++) {
+        cin>>arr1[i];
     }
-    for (int i = 0; i < n; i++) {
-        cin >> b[i];
+    for(int i=0; i<n; i++) {
+        cin>>arr2[i];
     }
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end(), greater<int>());
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += a[i] * b[i];
+    sort(arr1,arr1+n,greater<int>());
+    sort(arr2,arr2+n);
+    int ans=0;
+    for(int i=0; i<n; i++) {
+        ans+=(arr1[i]*arr2[i]);
     }
-    cout << sum;
+    cout<<ans;
 }
