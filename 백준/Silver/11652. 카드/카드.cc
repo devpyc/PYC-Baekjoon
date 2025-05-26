2a1664711d4069537bd13d44f659d10024bfa581
yechan <136340169+dpeyvc@users.noreply.github.com>
2025-05-26 21:06:34 +0900

    [Silver IV] Title: 카드, Time: 48 ms, Memory: 8360 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.cc
+++ 백준/Silver/11652. 카드/카드.cc
@@ -1,2 +1,27 @@
-#include<bits/stdc++.h>
-using namespace std;int main(){int n;cin>>n;map<long long,int>m;while(n--){long long x;cin>>x;m[x]++;}int mx=0;long long ans;for(auto&i:m)if(i.second>mx)mx=i.second,ans=i.first;cout<<ans;}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+    map<long long,int>arr;
+    for(int i=0; i<n; i++) {
+        long long x;
+        cin>>x;
+        arr[x]++;
+    }
+
+    long long ans=arr.begin()->first;
+    int cnt=arr.begin()->second;
+
+    for(auto& i:arr) {
+        if(i.second>cnt) {
+            cnt=i.second;
+            ans=i.first;
+        }
+    }
+    cout<<ans;
+}
