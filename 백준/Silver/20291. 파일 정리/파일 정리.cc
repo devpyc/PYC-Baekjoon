b41ea14f7df6c04ff187e0510861244daa60b7e0
yechan <136340169+dpeyvc@users.noreply.github.com>
2025-05-26 20:39:46 +0900

    [Silver III] Title: 파일 정리, Time: 244 ms, Memory: 10868 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.cc
+++ 백준/Silver/20291. 파일 정리/파일 정리.cc
@@ -2,20 +2,18 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int n;
     cin>>n;
     map<string,int>arr;
-    for(int i=0; i<n; i++) {
+    while(n--) {
         string s;
         cin>>s;
-        int m=s.find_first_of('.');
-        string cur=s.substr(m+1);
-        arr[cur]++;
+        s=s.substr(s.find('.')+1);
+        arr[s]++;
     }
-    for(auto& ans:arr) {
-        cout<<ans.first<<" "<<ans.second<<"\n";
+    int max=0;
+    string ans;
+    for(auto it=arr.begin(); it!=arr.end(); it++) {
+        cout<<it->first<<" "<<it->second<<"\n";
     }
 }
