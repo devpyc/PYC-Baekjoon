984e0b74aa9b34e7c53333fec1a342a5e4d96116
yechan <136340169+dpeyvc@users.noreply.github.com>
2025-05-26 20:59:11 +0900

    [Silver IV] Title: 대칭 차집합, Time: 384 ms, Memory: 39644 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,34 +1,34 @@
 #include <bits/stdc++.h>
-#define endl "\n"
 using namespace std;
 
-int main()
-{
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n,m;
     cin>>n>>m;
-    vector<int>a(n),b(m);
-
-    for(int i=0; i<n; ++i){
-        cin>>a[i];
+    set<int>a,b;
+    for(int i=0; i<n; i++) {
+        int x;
+        cin>>x;
+        a.insert(x);
     }
-    for(int i=0; i<m; ++i){
-        cin>>b[i];
+    for(int i=0; i<m; i++) {
+        int x;
+        cin>>x;
+        b.insert(x);
     }
+    set<int>ans;
 
-    sort(a.begin(),a.end());
-    sort(b.begin(),b.end());
-
-    int diff=0;
-
-    for(int i=0; i<n; ++i){
-        if(!binary_search(b.begin(),b.end(),a[i])){
-            diff++;
+    for(int i:a) {
+        if(b.find(i)==b.end()) {
+            ans.insert(i);
         }
     }
-    for(int i=0; i<m; ++i){
-        if(!binary_search(a.begin(),a.end(),b[i])){
-            diff++;
+    for(int i:b) {
+        if(a.find(i)==a.end()) {
+            ans.insert(i);
         }
     }
-    cout<<diff<<endl;
+    cout<<ans.size();
 }
