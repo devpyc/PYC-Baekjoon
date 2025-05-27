9652e27d5439f729375477dd1e3a3827bc13755d
yechan <136340169+dpeyvc@users.noreply.github.com>
2025-05-27 09:20:08 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -1,8 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
     int n;
     cin>>n;
-    (n%7==0||n%7==2)?cout<<"CY":cout<<"SK";
+    if(n%7==0||n%7==2) {
+        cout<<"CY";
+    }else {
+        cout<<"SK";
+    }
 }
