b429d45d8371cfc4a9eff0b95df1608f6290f5d4
yechan <136340169+dpeyvc@users.noreply.github.com>
2025-06-04 12:19:41 +0900

    [Bronze IV] Title: 3 つの整数 (Three Integers), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/18408. 3 つの整数 （Three Integers）/3 つの整数 （Three Integers）.cc
+++ 백준/Bronze/18408. 3 つの整数 （Three Integers）/3 つの整数 （Three Integers）.cc
@@ -1,19 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
 
-    int cnt1=0,cnt2=0;
-    int arr[3];
+    int a=0,b=0;
     for(int i=0; i<3; i++) {
-        cin>>arr[i];
-        if(arr[i]==1) {
-            cnt1++;
-        }else {
-            cnt2++;
-        }
+        int x;
+        cin>>x;
+        if(x==1)a++;
+        else b++;
     }
-    cout<<(cnt1<cnt2)+1;
+    (a>=b)?cout<<1:cout<<2;
 }
