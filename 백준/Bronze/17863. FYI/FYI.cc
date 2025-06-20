578c6c02fe5750f6a0997e8fa8edab3b70758cea
devpyc <136340169+devpyc@users.noreply.github.com>
2025-06-20 13:25:56 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string s;
+    cin>>s;
+    (s[0]=='5'&&s[1]=='5'&&s[2]=='5')?cout<<"YES":cout<<"NO";
+}
