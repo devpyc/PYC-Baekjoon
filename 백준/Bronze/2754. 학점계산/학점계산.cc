01020c7d44e5a991db85562d29714d9b96382719
devpyc <136340169+devpyc@users.noreply.github.com>
2025-06-25 09:41:16 +0900

    [Bronze III] Title: 학점계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.cc
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -1,36 +1,35 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
     string s;
     cin>>s;
-    if(s=="A+"){
-        cout<<"4.3";
-    }else if(s=="A0"){
-        cout<<"4.0";
-    }else if(s=="A-"){
-        cout<<"3.7";
-    }else if(s=="B+"){
-        cout<<"3.3";
-    }else if(s=="B0"){
-        cout<<"3.0";
-    }else if(s=="B-"){
-        cout<<"2.7";
-    }else if(s=="C+"){
-        cout<<"2.3";
-    }else if(s=="C0"){
-        cout<<"2.0";
-    }else if(s=="C-"){
-        cout<<"1.7";
-    }else if(s=="D+"){
-        cout<<"1.3";
-    }else if(s=="D0"){
-        cout<<"1.0";
-    }else if(s=="D-"){
-        cout<<"0.7";
-    }else if(s=="F"){
-        cout<<"0.0";
+    double ans;
+    if (s[0]=='A') {
+        ans=4;
+        if (s[1]=='+') ans+=0.3;
+        else if (s[1]=='-') ans-=0.3;
     }
+    else if (s[0]=='B') {
+        ans=3;
+        if (s[1]=='+') ans+=0.3;
+        else if (s[1]=='-') ans-=0.3;
+    }
+    else if (s[0]=='C') {
+        ans=2;
+        if (s[1]=='+') ans+=0.3;
+        else if (s[1]=='-') ans-=0.3;
+    }
+    else if (s[0]=='D') {
+        ans=1;
+        if (s[1]=='+') ans+=0.3;
+        else if (s[1]=='-') ans-=0.3;
+    }
+    else ans=0;
 
+    cout<<fixed;
+    cout.precision(1);
+    cout<<ans;
 }
