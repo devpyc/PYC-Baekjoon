daccbe413076aef8c105853729e8cc2eff0a9a7e
devpyc <136340169+devpyc@users.noreply.github.com>
2025-06-30 00:10:59 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 8 ms, Memory: 9712 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
@@ -2,14 +2,13 @@
 #define MOD 1'000'000'007
 using namespace std;
 
-long long dp[1000001]={0,1};
-
-int main() {
+int main(){
     cin.tie(0)->sync_with_stdio(0);
 
     int n;
     cin>>n;
-    for(int i=2; i<=n; i++) {
+    long long dp[1000001]={0,1};
+    for (int i=2; i<=n; i++) {
         dp[i]=(dp[i-1]+dp[i-2])%MOD;
     }
     cout<<dp[n];
