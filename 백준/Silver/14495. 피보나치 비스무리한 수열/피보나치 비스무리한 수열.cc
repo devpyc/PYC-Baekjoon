df9cd5bdb2cdbe27db47035e00be5cf988fdcf82
devpyc <136340169+devpyc@users.noreply.github.com>
2025-06-30 00:12:31 +0900

    [Silver IV] Title: 피보나치 비스무리한 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.cc
+++ 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.cc
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
+#define MOD 1'000'000'007
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+
     int n;
     cin>>n;
-    long long dp[n+1];
-    dp[1]=1,dp[2]=1,dp[3]=1;
-    for(int i=4; i<n+1; i++) {
+    long long dp[117]={1,1,1};
+    for (int i=3; i<=n; i++) {
         dp[i]=dp[i-1]+dp[i-3];
     }
-    cout<<dp[n];
+    cout<<dp[n-1];
 }
