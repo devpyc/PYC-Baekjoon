6bca030cc23fea90a5881a147e2a68b2752932e2
devpyc <136340169+devpyc@users.noreply.github.com>
2025-06-29 22:42:02 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.cc
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
 
     string s;
     cin>>s;
