9848f36eaf092a4df7bdf3676b2775e74f4ce24a
devpyc <136340169+devpyc@users.noreply.github.com>
2025-07-03 00:08:17 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -1,36 +1,29 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	int n,m;
-	cin>>n>>m;
-	n=n;
-	m=m;
-	int a[n][m];
-	int b[n][m];
-	
-	for(int i=0; i<n; i++)
-	{
-		for(int j=0; j<m; j++)
-		{
-			cin>>a[i][j];
-		}
-	}
-	for(int i=0; i<n; i++)
-	{
-		for(int j=0; j<m; j++)
-		{
-			cin>>b[i][j];
-		}
-	}
-	for(int i=0; i<n; i++)
-	{
-		for(int j=0; j<m; j++)
-		{
-			cout<<a[i][j]+b[i][j]<<" ";
-		}
-		cout<<"\n";
-	}
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,m;
+    cin>>n>>m;
+    int a[n][m];
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin>>a[i][j];
+        }
+    }
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            int x;
+            cin>>x;
+            a[i][j]+=x;
+        }
+    }
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cout<<a[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
 }
