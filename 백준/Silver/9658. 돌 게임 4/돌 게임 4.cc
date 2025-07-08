870fed5a0a0c92b6ce49eb12acade321a0754cea
devpyc <136340169+devpyc@users.noreply.github.com>
2025-07-08 10:52:13 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
-using namespace std;
-
 int main() {
     int n;
-    cin>>n;
-    (n%7==1||n%7==3)?cout<<"CY":cout<<"SK";
+    std::cin>>n;
+    std::cout<<(n%7==1||n%7==3?"CY":"SK");
 }
