d1c6087a7f6d36a59bc3f33fc8e93808ac0f573f
devpyc <136340169+devpyc@users.noreply.github.com>
2025-08-11 21:41:23 +0900

    [Silver V] Title: 배열 합치기, Time: 1460 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,31 +1,20 @@
 #include <bits/stdc++.h>
-#define endl "\n"
-#define vi vector<int>
-#define io ios_base::sync_with_stdio(false); cin.tie(nullptr);
 using namespace std;
 
-int main() {
-    io;
-    int n, m;
-    cin >> n >> m;
-
-    vi a(n);
-    vi b(m);
-    vi ans(n + m);
-
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int>arr;
+    for (int i=0; i<n; i++) {
+        int x;
+        cin>>x;
+        arr.push_back(x);
     }
-
-    for(int i = 0; i < m; i++) {
-        cin >> b[i];
+    for (int i=0; i<m; i++) {
+        int x;
+        cin>>x;
+        arr.push_back(x);
     }
-
-    merge(a.begin(), a.end(), b.begin(), b.end(), ans.begin());
-
-    for(int num : ans) {
-        cout << num << ' ';
-    }
-
-    return 0;
+    sort(arr.begin(),arr.end());
+    for (int i:arr) cout<<i<<" ";
 }
