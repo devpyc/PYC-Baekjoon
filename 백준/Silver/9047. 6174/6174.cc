548813b2236c3bf2a1f04e55b66c51e9503aa0fb
devpyc <136340169+devpyc@users.noreply.github.com>
2025-08-20 20:55:05 +0900

    [Silver V] Title: 6174, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9047. 6174/6174.cc
+++ 백준/Silver/9047. 6174/6174.cc
@@ -1,36 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(int n) {
-    string s=to_string(n);
-    while(s.length()<4) {
-        s="0"+s;
-    }
-
-    string asc=s;
-    string desc=s;
-    sort(asc.begin(),asc.end());
-    sort(desc.begin(),desc.end(),greater<int>());
-
-    int a=stoi(asc),b=stoi(desc);
-    return b-a;
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
     int t;
     cin>>t;
 
-    while(t--) {
-        int n;
-        cin>>n;
-        
+    while (t--) {
+        string s;
+        cin>>s;
+        while (s.size()<4) {
+            s="0"+s;
+        }
+
         int cnt=0;
-        while(n!=6174) {
-            n=solve(n);
+
+        while (s!="6174") {
+            string s1=s,s2=s;
+            sort(s1.begin(),s1.end(),greater<int>());
+            sort(s2.begin(),s2.end());
+            int ans=stoi(s1)-stoi(s2);
+            s=to_string(ans);
+            while (s.size()<4) {
+                s="0"+s;
+            }
             cnt++;
+            if (ans==0) break;
         }
         cout<<cnt<<"\n";
     }
