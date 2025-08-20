1cf9169e561524619ac442944d9d65eb535783e3
devpyc <136340169+devpyc@users.noreply.github.com>
2025-08-20 11:04:14 +0900

    [Silver III] Title: 시리얼 번호, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
@@ -1,45 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(const string& serial) {
-    int s = 0;
-    for (char c : serial) {
-        if (isdigit(c)) {
-            s += (c - '0');
-        }
+int sum(string s) {
+    int ans=0;
+    for (int i=0; i<s.size(); i++) {
+        if (isdigit(s[i])) ans+=s[i]-'0';
     }
-    return s;
+    return ans;
 }
 
-bool cmp(const string& a, const string& b) {
-    if (a.length() != b.length()) {
-        return a.length() < b.length();
+int comp(string a, string b) {
+    if (a.size()!=b.size()) return a.size()<b.size();
+    else {
+        if (sum(a)!=sum(b)) return sum(a)<sum(b);
+        else return a<b;
     }
-
-    int s1 = solve(a);
-    int s2 = solve(b);
-    if (s1 != s2) {
-        return s1 < s2;
-    }
-
-    return a < b;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
     int n;
-    cin >> n;
-
-    vector<string> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    cin>>n;
+    vector<string>arr(n);
+    for (int i=0; i<n; i++) {
+        cin>>arr[i];
     }
-
-    sort(arr.begin(), arr.end(), cmp);
-
-    for (const string& i : arr) {
-        cout<<i<<"\n";
+    sort(arr.begin(),arr.end(),comp);
+    for (int i=0; i<n; i++) {
+        cout<<arr[i]<<"\n";
     }
 }
