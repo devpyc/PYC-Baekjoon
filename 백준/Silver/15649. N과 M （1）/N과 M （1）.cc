e3d8259f659c7551d5fcb39a246c77dc31f9842d
devpyc <136340169+devpyc@users.noreply.github.com>
2025-08-21 22:59:01 +0900

    [Silver III] Title: N과 M (1), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+vector<int> seq;
+bool used[9];
+
+void dfs(int cnt) {
+    if (cnt == M) {
+        for (int num : seq) {
+            cout << num << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (!used[i]) {
+            used[i] = true;
+            seq.push_back(i);
+            dfs(cnt + 1);
+            seq.pop_back();
+            used[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+
+    dfs(0);
+
+    return 0;
+}
