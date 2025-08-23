df7e398cdb67ce8c4889f402099ea16487655527
devpyc <136340169+devpyc@users.noreply.github.com>
2025-08-23 19:58:00 +0900

    [Silver IV] Title: 배열 전체 탐색하기, Time: 80 ms, Memory: 2808 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.cc
+++ 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.cc
@@ -1,42 +1,34 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
-    int n, m;
-    cin >> n >> m;
+    int n,m;
+    cin>>n>>m;
 
-    vector<long long> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    vector<ll>arr(n);
+    for (int i=0; i<n; i++) {
+        cin>>arr[i];
     }
+    sort(arr.begin(),arr.end());
 
-    sort(arr.begin(), arr.end());
-
-    for (int i = 0; i < m; i++) {
-        int c;
-        cin >> c;
-
-        if (c == 1) {
-            long long k;
-            cin >> k;
-
-            auto it = lower_bound(arr.begin(), arr.end(), k);
-            cout << arr.end() - it << "\n";
-        } else if (c == 2) {
-            long long k;
-            cin >> k;
-
-            auto it = upper_bound(arr.begin(), arr.end(), k);
-            cout << arr.end() - it << "\n";
-        } else if (c == 3) {
-            long long i, j;
-            cin >> i >> j;
-            auto lower = lower_bound(arr.begin(), arr.end(), i);
-            auto upper = upper_bound(arr.begin(), arr.end(), j);
-            cout << upper - lower << "\n";
+    while (m--) {
+        int t;
+        cin>>t;
+        if (t==1) {
+            ll k;
+            cin>>k;
+            cout<<arr.end()-lower_bound(arr.begin(),arr.end(),k)<<"\n";
+        }else if (t==2) {
+            ll k;
+            cin>>k;
+            cout<<arr.end()-upper_bound(arr.begin(),arr.end(),k)<<"\n";
+        }else if (t==3) {
+            ll i,j;
+            cin>>i>>j;
+            cout<<upper_bound(arr.begin(),arr.end(),j)-lower_bound(arr.begin(),arr.end(),i)<<"\n";
         }
     }
 }
