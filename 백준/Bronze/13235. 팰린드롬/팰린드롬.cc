770c634cc3af48bb8eb5c9567f137b9b720e48c9
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-04 19:05:10 +0900

    [Bronze II] Title: 팰린드롬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/13235. 팰린드롬/팰린드롬.cc
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.cc
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     cin.tie(0)->sync_with_stdio(0);
 
     string s;
     cin>>s;
+
     string a=s;
     reverse(s.begin(),s.end());
-    (a==s)?cout<<"true":cout<<"false";
+    cout<<(a==s?"true":"false");
 }
