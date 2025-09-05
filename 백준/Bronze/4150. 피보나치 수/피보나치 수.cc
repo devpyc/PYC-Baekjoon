c33f5c03eaba998bb9b120aaf0bdb40d9cf5dfaa
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-05 09:48:08 +0900

    [Bronze I] Title: 피보나치 수, Time: 32 ms, Memory: 5604 KB -BaekjoonHub

--- 백준/Bronze/4150. 피보나치 수/피보나치 수.cc
+++ 백준/Bronze/4150. 피보나치 수/피보나치 수.cc
@@ -2,31 +2,32 @@
 using namespace std;
 
 string add(string a, string b){
-    string result;
-    int carry = 0;
-    int i = a.size() - 1;
-    int j = b.size() - 1;
+       string result;
+       int carry = 0;
+       int i = a.size() - 1;
+       int j = b.size() - 1;
 
-    while(i >= 0 || j >= 0 || carry > 0){
-        int sum = carry;
-        if(i >= 0) sum += a[i--] - '0';
-        if(j >= 0) sum += b[j--] - '0';
-        carry = sum / 10;
-        sum = sum % 10;
-        result += to_string(sum);
-    }
-    reverse(result.begin(), result.end());
-    return result;
+       while(i >= 0 || j >= 0 || carry > 0){
+           int sum = carry;
+           if(i >= 0) sum += a[i--] - '0';
+           if(j >= 0) sum += b[j--] - '0';
+           carry = sum / 10;
+           sum = sum % 10;
+           result += to_string(sum);
+       }
+       reverse(result.begin(), result.end());
+       return result;
 }
 
-int main(){
+int main()
+{
     cin.tie(0)->sync_with_stdio(0);
 
     int n;
     cin>>n;
+
     vector<string>dp(n+1);
-    dp[0]="0";
-    dp[1]="1";
+    dp[0]="0",dp[1]="1";
     for (int i=2; i<=n; i++) {
         dp[i]=add(dp[i-1],dp[i-2]);
     }
