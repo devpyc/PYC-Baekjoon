2f70c530a2e678ea73bb7529308db10325bb0bce
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-11 15:32:50 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 304 ms, Memory: 5800 KB -BaekjoonHub

--- 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.cc
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.cc
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
+    cin.tie(0)->sync_with_stdio(0);
+
     int t;
     cin>>t;
-    while(t--) {
+    while (t--) {
         int n;
         cin>>n;
         int arr[n];
-        for(int i=0; i<n; i++) {
+        for (int i=0; i<n; i++) {
             cin>>arr[i];
         }
         cout<<*min_element(arr,arr+n)<<" "<<*max_element(arr,arr+n)<<"\n";
