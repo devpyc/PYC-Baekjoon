4e63f9bcb9619ed866dc9d4f6eba6c14f47382b3
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-11 11:26:41 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 8 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.cc
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.cc
@@ -1,11 +1,9 @@
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-long long sum(vector<int> &a) {
-    long long ans = 0;
-    for (int i = 0; i < a.size(); i++) {
-        ans += a[i];
+long long sum(std::vector<int> &a) {
+	long long ans = 0;
+    for(int i:a){
+        ans+=i;
     }
-    return ans;
+	return ans;
 }
