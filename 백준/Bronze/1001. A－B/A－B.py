f1479915dd2b9d22ac2ebbc73b0ac569c145cac8
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-16 15:51:36 +0900

    [Bronze V] Title: A-B, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.py
+++ 백준/Bronze/1001. A－B/A－B.py
@@ -1,10 +1,5 @@
 import sys
 
-input = sys.stdin.readline()
-
-list = list(map(int, input.split()))
-
-a=list[0]
-b=list[1]
-
-print(list[0]-list[1])
+input=sys.stdin.readline
+n,m=map(int,input().split())
+print(n-m)
