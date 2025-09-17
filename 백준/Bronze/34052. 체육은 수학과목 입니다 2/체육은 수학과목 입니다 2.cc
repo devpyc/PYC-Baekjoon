5c6152faec353a3846b5f78b769d5788d23194b5
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-17 13:14:37 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    cout<<((1500-a-b-c-d>=0)?"Yes":"No");
+}
