7d8b05fd59cd94a53a91b7520ab5b58774cf34a0
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-17 15:39:15 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    for (int i:arr) {
+        cout<<i<<"\n";
+    }
+}
