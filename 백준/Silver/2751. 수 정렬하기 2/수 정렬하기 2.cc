4ca527ba9a85d82ade6fd351da197dbc94076f32
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-17 15:43:01 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 256 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
     int n;
     cin>>n;
     int arr[n];
-    for(int i=0; i<n; i++) {
+    for (int i=0; i<n; i++) {
         cin>>arr[i];
     }
     sort(arr,arr+n);
-
-    for(int i=0; i<n; i++) {
-        cout<<arr[i]<<"\n";
+    for (int i:arr) {
+        cout<<i<<"\n";
     }
 }
