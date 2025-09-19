99ae396a5eaa19fd31dfa04ce95b05af3f27348b
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-19 11:44:45 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool comp(pair<int,int>&a, pair<int,int>&b) {
+    if (a.second==b.second) return a.first<b.first;
+    return a.second<b.second;
+}
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
     int n;
     cin>>n;
-
     vector<pair<int,int>>arr;
-    for(int i=0; i<n; i++) {
+    for (int i=0; i<n; i++) {
         int x,y;
         cin>>x>>y;
-        arr.push_back({y,x});
+        arr.push_back({x,y});
     }
-    sort(arr.begin(),arr.end());
+    sort(arr.begin(),arr.end(),comp);
 
-    for(int i=0; i<arr.size(); i++) {
-        cout<<arr[i].second<<" "<<arr[i].first<<"\n";
+    for (int i=0; i<n; i++) {
+        cout<<arr[i].first<<" "<<arr[i].second<<"\n";
     }
 }
