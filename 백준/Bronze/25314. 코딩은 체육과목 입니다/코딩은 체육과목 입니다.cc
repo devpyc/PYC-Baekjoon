7a72fc82632277ef3bd6a7b21be4ab3984fd9b4d
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-19 11:08:40 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n;
+    cin>>n;
+    n/=4;
+
+    while (n--) {
+        cout<<"long ";
+    }
+    cout<<"int";
+}
