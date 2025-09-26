2132163e56a01df8c51c1e4b436c6e374d09c916
devpyc <136340169+devpyc@users.noreply.github.com>
2025-09-26 14:47:26 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,39 +1,39 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int n, root, del, cnt;
-vector<int> tree[51];
+int n,r,p,cnt;
+vector<int>tree[51];
 
-void dfs(int node) {
-    if (node == del) return;
-    if (tree[node].empty()) {
+void dfs(int n) {
+    if (n==p) return;
+    if (tree[n].empty()) {
         cnt++;
         return;
     }
-    if (tree[node].size() == 1 && tree[node][0] == del) {
+    if (tree[n].size()==1&&tree[n][0]==p) {
         cnt++;
         return;
     }
-    for (int i = 0; i < tree[node].size(); i++) {
-        dfs(tree[node][i]);
+    for (int i=0; i<tree[n].size(); i++) {
+        dfs(tree[n][i]);
     }
 }
 
 int main() {
-    cin >> n;
-    for (int i = 0; i < n; i++) {
+    cin.tie(0)->sync_with_stdio(0);
+
+    cin>>n;
+    for (int i=0; i<n; i++) {
         int a;
-        cin >> a;
-        if (a == -1) root = i;
+        cin>>a;
+        if (a==-1) r=i;
         else tree[a].push_back(i);
     }
-    cin >> del;
-    if (del == root) {
-        cout << 0;
+    cin>>p;
+    if (p==r) {
+        cout<<0;
         return 0;
     }
-    dfs(root);
-    cout << cnt;
-    return 0;
+    dfs(r);
+    cout<<cnt;
 }
