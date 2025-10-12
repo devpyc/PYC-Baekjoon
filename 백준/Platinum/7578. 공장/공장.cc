677de0e1596185e5cb1017fe77e547ca403edf42
devpyc <136340169+devpyc@users.noreply.github.com>
2025-10-12 18:39:18 +0900

    [Platinum V] Title: 공장, Time: 668 ms, Memory: 45036 KB -BaekjoonHub

--- 백준/Platinum/7578. 공장/공장.cc
+++ 백준/Platinum/7578. 공장/공장.cc
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
-#define int long long
-#define endl "\n"
+#define ll long long
+#define MAX_N 500001
 using namespace std;
-
-const int MAX_N = 500000 + 1;
 
 class FenwickTree {
 private:
-    vector<int> tree;
-    int size;
+    vector<ll> tree;
+    ll size;
+
 public:
-    FenwickTree(int n) : size(n) {
-        tree.resize(n+1, 0);
+    FenwickTree(ll n) : size(n) {
+        tree.resize(n + 1, 0);
     }
 
-    void update(int idx, int val) {
+    void update(ll idx, ll val) {
         while (idx <= size) {
             tree[idx] += val;
             idx += (idx & -idx);
         }
     }
 
-    int query(int idx) {
-        int sum = 0;
+    int query(ll idx) {
+        ll sum = 0;
         while (idx > 0) {
             sum += tree[idx];
             idx -= (idx & -idx);
@@ -31,33 +30,29 @@ public:
     }
 };
 
-int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
 
-    int n;
+    ll n;
     cin >> n;
-    vector<int> a(n+1), b(n+1);
-    map<int, int> indexMap;
+    vector<ll> a(n + 1), b(n + 1);
+    map<ll, ll> arr;
 
-    for (int i = 1; i <= n; i++) {
+    for (ll i = 1; i <= n; i++) {
         cin >> a[i];
     }
-    for (int i = 1; i <= n; i++) {
+    for (ll i = 1; i <= n; i++) {
         cin >> b[i];
-        indexMap[b[i]] = i;
+        arr[b[i]] = i;
     }
 
-    FenwickTree bit(n);
-    long long inversions = 0;
+    FenwickTree tree(n);
+    ll ans = 0;
 
-    for (int i = 1; i <= n; i++) {
-        int pos = indexMap[a[i]];
-        inversions += i - 1 - bit.query(pos);
-        bit.update(pos, 1);
+    for (ll i = 1; i <= n; i++) {
+        ll pos = arr[a[i]];
+        ans += i - 1 - tree.query(pos);
+        tree.update(pos, 1);
     }
-
-    cout << inversions << endl;
-
-    return 0;
+    cout << ans << "\n";
 }
