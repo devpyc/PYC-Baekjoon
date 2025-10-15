3ca4ddd392dd87c859afef9883ee5424ea54c84c
devpyc <136340169+devpyc@users.noreply.github.com>
2025-10-15 10:15:18 +0900

    [Platinum III] Title: 북서풍, Time: 444 ms, Memory: 5364 KB -BaekjoonHub

--- 백준/Platinum/5419. 북서풍/북서풍.cc
+++ 백준/Platinum/5419. 북서풍/북서풍.cc
@@ -1,47 +1,44 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define endl "\n"
-#define io ios_base::sync_with_stdio(false);cin.tie(nullptr);
 using namespace std;
 
 struct Node {
     int x, y;
-    Node(int x, int y) : x(x), y(y) {}
+    Node(int x, int y) : x(x), y(y) {
+    }
 };
 
-ll query(vector<int>& tree, int start, int end, int left, int right, int node) {
+ll query(vector<int> &tree, int start, int end, int left, int right, int node) {
     if (left > end || right < start) return 0;
     if (left <= start && end <= right) return tree[node];
     int mid = (start + end) / 2;
     return query(tree, start, mid, left, right, node * 2) + query(tree, mid + 1, end, left, right, node * 2 + 1);
 }
 
-int update(vector<int>& tree, int start, int end, int target, int node) {
+int update(vector<int> &tree, int start, int end, int target, int node) {
     if (target < start || target > end) return tree[node];
-    if (start == end) {
-        return tree[node] += 1;
-    }
+    if (start == end) return tree[node] += 1;
     int mid = (start + end) / 2;
     return tree[node] = update(tree, start, mid, target, node * 2) + update(tree, mid + 1, end, target, node * 2 + 1);
 }
 
 ll solve() {
-    int N;
-    cin >> N;
-    vector<Node> list;
-    for (int i = 0; i < N; i++) {
+    int n;
+    cin >> n;
+    vector<Node> arr;
+    for (int i = 0; i < n; i++) {
         int x, y;
         cin >> x >> y;
-        list.emplace_back(x, y);
+        arr.emplace_back(x, y);
     }
 
-    sort(list.begin(), list.end(), [](const Node& n1, const Node& n2) {
+    sort(arr.begin(), arr.end(), [](const Node &n1, const Node &n2) {
         return n1.y > n2.y;
     });
 
     int ny = 1;
-    int prev = list[0].y;
-    for (auto& n : list) {
+    int prev = arr[0].y;
+    for (auto &n: arr) {
         if (n.y == prev) {
             n.y = ny;
             continue;
@@ -50,14 +47,14 @@ ll solve() {
         n.y = ++ny;
     }
 
-    sort(list.begin(), list.end(), [](const Node& n1, const Node& n2) {
+    sort(arr.begin(), arr.end(), [](const Node &n1, const Node &n2) {
         if (n1.x != n2.x) return n1.x < n2.x;
         return n1.y < n2.y;
     });
 
     ll ans = 0;
-    vector<int> tree(N * 4, 0);
-    for (const auto& node : list) {
+    vector<int> tree(n * 4, 0);
+    for (const auto &node: arr) {
         ans += query(tree, 1, ny, 1, node.y, 1);
         update(tree, 1, ny, node.y, 1);
     }
@@ -65,11 +62,11 @@ ll solve() {
 }
 
 int main() {
-    io;
+    cin.tie(0)->sync_with_stdio(0);
+
     int t;
     cin >> t;
-    for (int i = 0; i < t; i++) {
-        cout << solve() << endl;
+    while (t--) {
+        cout << solve() << "\n";
     }
-    return 0;
 }
