469abf72b28be11db1c0d2c372285d14d4113a19
devpyc <136340169+devpyc@users.noreply.github.com>
2025-10-16 12:09:32 +0900

    [Silver V] Title: 나이순 정렬, Time: 40 ms, Memory: 9848 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -6,8 +6,7 @@ bool cmp(const pair<int,string>&a, const pair<int, string>& b) {
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
     int n;
     cin>>n;
