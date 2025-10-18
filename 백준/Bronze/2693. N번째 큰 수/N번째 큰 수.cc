18be96888848eed82bb8e877ce9ef669f3300399
devpyc <136340169+devpyc@users.noreply.github.com>
2025-10-18 14:45:23 +0900

    [Bronze I] Title: N번째 큰 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.cc
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
     int t;
     cin>>t;
-    int arr[10];
-    while(t--){
-        for(int i=0; i<10; i++){
+    while (t--) {
+        int arr[10];
+        for (int i=0; i<10; i++) {
             cin>>arr[i];
         }
-        sort(arr,arr+10);
-        cout<<arr[7]<<"\n";
+        sort(arr,arr+10,greater<int>());
+        cout<<arr[2]<<"\n";
     }
 }
