c2f98c44971d38934a3a80662f2b508b341ef1ea
devpyc <136340169+devpyc@users.noreply.github.com>
2025-10-21 00:11:15 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 84 ms, Memory: 9648 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -1,55 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> graph[100001];
-int visited[100001] = {0,};
-int order[100001] = {0,};
-int cnt = 1;
+vector<int>graph[100001];
+bool visited[100001];
+int ans[100001];
 
 void bfs(int start) {
-    queue<int> q;
+    queue<int>q;
     q.push(start);
-    visited[start] = 1;
-    order[start] = cnt++;
+    visited[start]=true;
+    int cnt=1;
+    ans[start]=cnt;
 
-    while(!q.empty()) {
-        int current = q.front();
+    while (!q.empty()) {
+        int x=q.front();
         q.pop();
 
-        for(int i = 0; i < graph[current].size(); i++) {
-            int next = graph[current][i];
-            if(visited[next] == 0) {
-                q.push(next);
-                visited[next] = 1;
-                order[next] = cnt++;
+        for (int i:graph[x]) {
+            if (!visited[i]) {
+                q.push(i);
+                visited[i]=true;
+                ans[i]=++cnt;
             }
         }
     }
 }
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
-    int N, M, R;
-    cin >> N >> M >> R;
-
-    for(int i = 0; i < M; i++) {
-        int u, v;
-        cin >> u >> v;
+    int n,m,r;
+    cin>>n>>m>>r;
+    for (int i=0; i<m; i++) {
+        int u,v;
+        cin>>u>>v;
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
-
-    for(int i = 1; i <= N; i++) {
-        sort(graph[i].begin(), graph[i].end());
+    for (int i=1; i<=n; i++) {
+        sort(graph[i].begin(),graph[i].end());
     }
-
-    bfs(R);
-
-    for(int i = 1; i <= N; i++) {
-        cout << order[i] << '\n';
+    bfs(r);
+    for (int i=1; i<=n; i++) {
+        cout<<ans[i]<<"\n";
     }
-
-    return 0;
 }
