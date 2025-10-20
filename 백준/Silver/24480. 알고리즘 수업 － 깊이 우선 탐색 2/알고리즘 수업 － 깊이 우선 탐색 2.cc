e7a2f5bce0d09969223a3296646d0a379571b800
devpyc <136340169+devpyc@users.noreply.github.com>
2025-10-20 22:13:41 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 88 ms, Memory: 11024 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>graph[100001];
+bool visited[100001];
+int ans[100001];
+int cnt=0;
+
+void dfs(int x) {
+    ans[x]=++cnt;
+    visited[x]=true;
+    for (int i:graph[x]) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,m,r;
+    cin>>n>>m>>r;
+    for (int i=0; i<m; i++) {
+        int u,v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    for (int i=1; i<=n; i++) {
+        sort(graph[i].begin(),graph[i].end(),greater<int>());
+    }
+    dfs(r);
+    for (int i=1; i<=n; i++) {
+        cout<<ans[i]<<"\n";
+    }
+}
