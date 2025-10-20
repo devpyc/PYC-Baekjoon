348b1d1d3c95302292daeae953e4ad869a98698b
devpyc <136340169+devpyc@users.noreply.github.com>
2025-10-21 00:11:56 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 80 ms, Memory: 8936 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>graph[100001];
+bool visited[100001];
+int ans[100001];
+
+void bfs(int start) {
+    queue<int>q;
+    q.push(start);
+    visited[start]=true;
+    int cnt=1;
+    ans[start]=cnt;
+
+    while (!q.empty()) {
+        int x=q.front();
+        q.pop();
+
+        for (int i:graph[x]) {
+            if (!visited[i]) {
+                q.push(i);
+                visited[i]=true;
+                ans[i]=++cnt;
+            }
+        }
+    }
+}
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,m,r;
+    cin>>n>>m>>r;
+    for (int i=0; i<m; i++) {
+        int u,v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    for (int i=1; i<=n; i++) {
+        sort(graph[i].begin(),graph[i].end(),greater<int>());
+    }
+    bfs(r);
+    for (int i=1; i<=n; i++) {
+        cout<<ans[i]<<"\n";
+    }
+}
