89f368af3e22c15a878415a631d96fc6f7122048
devpyc <136340169+devpyc@users.noreply.github.com>
2025-10-27 20:38:46 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -1,55 +1,41 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-int main(){
-   ios::sync_with_stdio(false);
-   cin.tie(nullptr);
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
 
-   int N;
-   cin >> N;
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+    int cnt=0;
+    sort(arr,arr+n);
+    for (int i=0; i<n; i++) {
+        bool check=false;
+        int l=0,r=n-1;
+        while (l<r) {
+            if (l==i) {
+                l++;
+                continue;
+            }
+            if (r==i) {
+                r--;
+                continue;
+            }
 
-   vector<ll> A(N);
-   for(int i=0;i<N;i++) {
-       cin >> A[i];
-   }
-
-   sort(A.begin(), A.end());
-
-   int cnt = 0;
-   for(int i=0;i<N;i++){
-       bool is_good = false;
-       int left = 0;
-       int right = N-1;
-       
-       while(left < right) {
-           if(left == i) {
-               left++;
-               continue;
-           }
-           if(right == i) {
-               right--;
-               continue;
-           }
-           
-           ll sum = A[left] + A[right];
-           if(sum == A[i]) {
-               is_good = true;
-               break;
-           }
-           else if(sum < A[i]) {
-               left++;
-           }
-           else {
-               right--;
-           }
-       }
-       
-       if(is_good) {
-           cnt++;
-       }
-   }
-
-   cout << cnt;
-   return 0;
+            int sum=arr[l]+arr[r];
+            if (sum==arr[i]) {
+                check=true;
+                break;
+            } else if (sum<arr[i]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        if (check) cnt++;
+    }
+    cout<<cnt;
 }
