811fdc2770cc6cf324810088dcd8b461c71cf7bf
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-02 14:00:59 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int lis(const vector<int>&arr) {
+    vector<int>lis;
+    for (int x:arr) {
+        auto it=std::lower_bound(lis.begin(),lis.end(),x,greater<int>());
+        if (it==lis.end()) lis.push_back(x);
+        else *it=x;
+    }
+    return static_cast<int>(lis.size());
+}
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
     int n;
     cin>>n;
     vector<int>arr(n);
-    for(int i=0; i<n; i++) {
+    for (int i=0; i<n; i++) {
         cin>>arr[i];
     }
-
-    vector<int>dp(n,1);
-    for(int i=1; i<n; i++) {
-        for(int j=0; j<i; j++) {
-            if(arr[j]>arr[i]) {
-                dp[i]=max(dp[i],dp[j]+1);
-            }
-        }
-    }
-    cout<<*max_element(dp.begin(),dp.end());
+    cout<<lis(arr);
 }
