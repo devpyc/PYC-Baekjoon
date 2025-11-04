5e0fffcd3bd3f30b77eafb88d89cea034aec380d
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-04 10:48:32 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 140 ms, Memory: 12200 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int lis(const vector<int>&arr) {
+int lis(vector<int>&arr) {
     vector<int>lis;
     for (int x:arr) {
-        auto it=std::lower_bound(lis.begin(),lis.end(),x);
+        auto it=lower_bound(lis.begin(),lis.end(),x);
         if (it==lis.end()) lis.push_back(x);
         else *it=x;
     }
-    return static_cast<int>(lis.size());
+    return (int)lis.size();
 }
 
 int main() {
@@ -16,10 +16,10 @@ int main() {
 
     int n;
     cin>>n;
-
     vector<int>arr(n);
     for (int i=0; i<n; i++) {
         cin>>arr[i];
     }
+
     cout<<lis(arr);
 }
