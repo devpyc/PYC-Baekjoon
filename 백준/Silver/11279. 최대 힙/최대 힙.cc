83db86330ac4aff6d11d9162bb1df17f7cff2d65
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-04 22:31:48 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n;
+    cin>>n;
+    priority_queue<int>pq;
+    while (n--) {
+        int x;
+        cin>>x;
+        if (x!=0) {
+            pq.push(x);
+        }else {
+            if (pq.empty()) cout<<0<<"\n";
+            else {
+                cout<<pq.top()<<"\n";
+                pq.pop();
+            }
+        }
+    }
+}
