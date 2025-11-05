bb8965e8a99131b4dd349f4fadb6e291b45cc711
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-06 01:26:46 +0900

    [Silver II] Title: 민균이의 계략, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lis(vector<int>&arr) {
+    vector<int>lis;
+    for (auto x:arr) {
+        auto it=lower_bound(lis.begin(),lis.end(),x);
+        if (it==lis.end()) lis.push_back(x);
+        else *it=x;
+    }
+    return (int)lis.size();
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for (int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    cout<<lis(arr);
+}
