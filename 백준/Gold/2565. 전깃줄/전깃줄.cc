96037df88b45aeaca172c48c5d61c87ef6171403
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-06 01:15:04 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.cc
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -1,38 +1,26 @@
 #include <bits/stdc++.h>
-#define X first
-#define Y second
 using namespace std;
 
+int lis(vector<pair<int,int>>&arr) {
+    vector<int>lis;
+    for (auto p:arr) {
+        int x=p.second;
+        auto it=lower_bound(lis.begin(),lis.end(),x);
+        if (it==lis.end()) lis.push_back(x);
+        else *it=x;
+    }
+    return (int)lis.size();
+}
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
+    cin.tie(0)->sync_with_stdio(0);
+
     int n;
-    cin >> n;
-
-    vector<pair<int, int>> l(n);
-    vector<int> dp(n, 1);
-
-    for(int i = 0; i < n; i++) {
-        cin >> l[i].X >> l[i].Y;
+    cin>>n;
+    vector<pair<int,int>>arr(n);
+    for (int i=0; i<n; i++) {
+        cin>>arr[i].first>>arr[i].second;
     }
-
-    sort(l.begin(), l.end());
-
-    for(int i = 1; i < n; i++) {
-        for(int j = 0; j < i; j++) {
-            if(l[i].Y > l[j].Y) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
-        }
-    }
-
-    int ans = 0;
-    for(int i = 0; i < n; i++) {
-        ans = max(ans, dp[i]);
-    }
-
-    cout << n - ans << endl;
-
-    return 0;
+    sort(arr.begin(),arr.end());
+    cout<<n-lis(arr);
 }
