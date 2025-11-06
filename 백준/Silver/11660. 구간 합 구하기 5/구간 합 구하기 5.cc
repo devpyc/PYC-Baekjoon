f2687dac69d46ec38c96562d01a070fe7d87c204
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-06 20:17:20 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 108 ms, Memory: 6264 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,33 +1,23 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(0)->sync_with_stdio(0);
 
-    int N, M;
-    cin >> N >> M;
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>arr(n+1,vector<int>(n+1,0));
 
-    vector<vector<int>> arr(N + 1, vector<int>(N + 1, 0));
-    vector<vector<int>> dp(N + 1, vector<int>(N + 1, 0));
-
-    // 배열 입력
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j <= N; ++j) {
-            cin >> arr[i][j];
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            int x;
+            cin>>x;
+            arr[i][j]=x+arr[i-1][j]+arr[i][j-1]-arr[i-1][j-1];
         }
     }
-
-    // 구간 합 구하기
-    for (int i = 0; i < M; ++i) {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-
-        int result = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
-        cout << result << "\n";
+    for (int i=0; i<m; i++) {
+        int x1,y1,x2,y2;
+        cin>>x1>>y1>>x2>>y2;
+        cout<<arr[x2][y2]-arr[x1-1][y2]-arr[x2][y1-1]+arr[x1-1][y1-1]<<"\n";
     }
-    return 0;
 }
