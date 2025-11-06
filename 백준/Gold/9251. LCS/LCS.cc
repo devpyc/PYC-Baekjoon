82e776b1676eec56c3c7406a774126d9da439f03
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-06 19:31:53 +0900

    [Gold V] Title: LCS, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,31 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    string str1, str2;
-    cin >> str1 >> str2;
-
-    int len1 = str1.length();
-    int len2 = str2.length();
-    
-    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
-
-    for (int i = 1; i <= len1; ++i) {
-        for (int j = 1; j <= len2; ++j) {
-            if (str1[i - 1] == str2[j - 1]) {
-                // 현재 문자가 같다면, 이전 문자까지의 LCS 길이에 1을 더해준다.
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                // 현재 문자가 다르다면, 이전에서의 LCS 중에서 더 큰 값을 가져온다.
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
+int lcs(string& a, string& b) {
+    vector<int>dp(b.size()+1);
+    for (char x:a) {
+        int res=0;
+        for (int j=0; j<b.size(); j++) {
+            int tmp=dp[j+1];
+            dp[j+1]=x==b[j]?res+1:max(dp[j+1],dp[j]);
+            res=tmp;
         }
     }
-    // str1과 str2의 LCS 길이 출력
-    cout << dp[len1][len2] << endl;
+    return dp.back();
+}
 
-    return 0;
+int main() {
+    string a,b;
+    cin>>a>>b;
+    cout<<lcs(a,b);
 }
