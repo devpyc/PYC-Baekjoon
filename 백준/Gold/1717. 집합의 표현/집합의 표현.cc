506ed73ccfdac3b64b032178436a8d9f4c9fbf76
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-08 18:06:48 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,47 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct UnionFind {
-    vector<int> parent, Rank;
-    UnionFind(int n): parent(n+1), Rank(n+1, 0) {
-        for (int i=0; i<=n; i++) parent[i]=i;
-    }
+vector<int>parent;
 
-    int find(int x) {
-        if (parent[x]==x) return x;
-        return parent[x]=find(parent[x]);
-    }
-    void unite(int a, int b) {
-        a=find(a);
-        b=find(b);
-        if(a==b) return;
-        if(Rank[a]<Rank[b]) parent[a]=b;
-        else if(Rank[a]>Rank[b]) parent[b]=a;
-        else{
-            parent[b]=a;
-            Rank[a]++;
-        }
-    }
-    bool check(int a, int b) {
-        return find(a)==find(b);
-    }
-};
+int find(int x) {
+    return parent[x]==x?x:parent[x]=find(parent[x]);
+}
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+void unite(int a, int b) {
+    a=find(a);
+    b=find(b);
+    if (a!=b) parent[b]=a;
+}
+
+bool same(int a, int b) {
+    return find(a)==find(b);
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
 
     int n,m;
     cin>>n>>m;
-    UnionFind arr(n);
 
-    while(m--) {
-        int a,b,c;
-        cin>>a>>b>>c;
-        if(a==0){
-            arr.unite(b, c);
-        } else if(a==1){
-            cout<<(arr.check(b, c)?"YES\n":"NO\n");
-        }
+    parent.resize(n+1);
+    for (int i=0; i<=n; i++) {
+        parent[i]=i;
+    }
+    while (m--) {
+        int x,a,b;
+        cin>>x>>a>>b;
+        if (x) cout<<(same(a,b)?"YES":"NO")<<"\n";
+        else unite(a,b);
     }
 }
