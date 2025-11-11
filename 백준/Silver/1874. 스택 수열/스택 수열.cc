8fb0daad693f6230ce6c6634fbfdf6f77f785ae9
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-11 23:04:50 +0900

    [Silver II] Title: 스택 수열, Time: 24 ms, Memory: 2624 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,33 +1,32 @@
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-
 int main() {
-	int N, nownum = 1, input;
-	stack<int> stk;
-	string ans = "+";
-	stk.push(nownum++);
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> input;
-		while (stk.empty() || stk.top() < input) {
-			ans += "+";
-			stk.push(nownum++);
-		}
-		if (stk.top() == input) {
-			ans += "-";
-			stk.pop();
-		}
-		else {
-			ans = "NO";
-			cout << ans << endl;
-			return 0;
-		}
-	}
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i] << '\n';
-	}
-	return 0;
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,x=2;
+    cin>>n;
+    stack<int>st;
+    string ans="+";
+    st.push(1);
+    for (int i=0; i<n; i++) {
+        int num;
+        cin>>num;
+        while (st.empty()||st.top()<num) {
+            ans+="+";
+            st.push(x);
+            x++;
+        }
+        if (st.top()==num) {
+            ans+="-";
+            st.pop();
+        }else {
+            ans="NO";
+            cout<<ans<<"\n";
+            return 0;
+        }
+    }
+    for (char c:ans) {
+        cout<<c<<"\n";
+    }
 }
