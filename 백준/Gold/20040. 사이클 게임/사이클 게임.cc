7dffef3dd16c5552894590f2a325ac836f5cdeed
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-13 23:04:58 +0900

    [Gold IV] Title: 사이클 게임, Time: 92 ms, Memory: 6452 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.cc
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -1,51 +1,45 @@
-#include <iostream>  
-#include <vector>  
-  
-using namespace std;  
-  
-vector<int> parent;  
-  
-int find(int x) {  
-    if (parent[x] == x) {  
-        return x;  
-    }  
-    return parent[x] = find(parent[x]);  
-}  
-  
-void unionSets(int a, int b) {  
-    int rootA = find(a);  
-    int rootB = find(b);  
-  
-    if (rootA != rootB) {  
-        parent[rootB] = rootA;  
-    }  
-}  
-  
-int main() {  
-    int n, m;  
-    cin >> n >> m;  
-  
-    parent.resize(n);  
-    for (int i = 0; i < n; i++) {  
-        parent[i] = i;  
-    }  
-  
-    int answer = 0;  
-  
-    for (int i = 1; i <= m; i++) {  
-        int a, b;  
-        cin >> a >> b;  
-  
-        if (find(a) == find(b)) {  
-            // 현재 간선이 이미 같은 집합에 속하는 두 노드를 연결하면 사이클이 형성됨  
-            answer = i;  
-            break;  
-        }  
-  
-        unionSets(a, b);  
-    }  
-  
-    cout << answer << "\n";  
-  
-    return 0;  
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>parent;
+
+int find(int x) {
+    if (parent[x]==x) {
+        return x;
+    }else {
+        return parent[x]=find(parent[x]);
+    }
+}
+
+void unite(int a, int b) {
+    a=find(a);
+    b=find(b);
+    if (a!=b) parent[b]=a;
+}
+
+bool same(int a, int b) {
+    return find(a)==find(b);
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,m;
+    cin>>n>>m;
+    parent.resize(n+1);
+    for (int i=1; i<=n; i++) {
+        parent[i]=i;
+    }
+
+    bool check=false;
+    for (int i=1; i<=m; i++) {
+        int u,v;
+        cin>>u>>v;
+        if (same(u,v)) {
+            cout<<i;
+            return 0;
+        }
+        unite(u,v);
+    }
+    cout<<0;
 }
