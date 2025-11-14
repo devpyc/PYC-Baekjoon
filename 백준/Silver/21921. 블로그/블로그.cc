201ca1c476f04359b3a6fa4aa419a542f3059b26
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-14 10:55:01 +0900

    [Silver III] Title: 블로그, Time: 20 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.cc
+++ 백준/Silver/21921. 블로그/블로그.cc
@@ -4,27 +4,30 @@ using namespace std;
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    int n,m;
-    cin>>n>>m;
+    int n,k;
+    cin>>n>>k;
     int arr[n];
     int sum=0;
     for (int i=0; i<n; i++) {
         cin>>arr[i];
+        if (i<k) {
+            sum+=arr[i];
+        }
     }
-    for (int i=0; i<m; i++) {
-        sum+=arr[i];
-    }
-    int MIN=sum;
+    int MAX=sum;
     int cnt=1;
-    for (int i=m; i<n; i++) {
-        sum+=arr[i]-arr[i-m];
-        if (MIN<sum) {
-            MIN=sum;
+    for (int i=k; i<n; i++) {
+        sum+=arr[i]-arr[i-k];
+        if (sum>MAX) {
+            MAX=sum;
             cnt=1;
-        }else if (MIN==sum) {
+        }else if (sum==MAX){
             cnt++;
         }
     }
-    if (MIN==0) cout<<"SAD";
-    else cout<<MIN<<"\n"<<cnt;
+    if (MAX!=0) {
+        cout<<MAX<<"\n"<<cnt;
+    }else {
+        cout<<"SAD";
+    }
 }
