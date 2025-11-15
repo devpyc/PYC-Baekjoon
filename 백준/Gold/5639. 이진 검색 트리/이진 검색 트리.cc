d36b8283c92ce17cf81d8de98f83f74defc42988
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-15 10:47:12 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 392 ms, Memory: 2604 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -1,59 +1,35 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
-struct NodeType{ 
-    int info;
-    NodeType *right;
-    NodeType *left;
+
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+
+    Node(int x):key(x),left(NULL),right(NULL){}
 };
-void InOrder(NodeType* tr ,queue<int>& q){
-    if(tr != NULL){
-        InOrder(tr->left, q);
-        InOrder(tr->right, q);
-        q.push(tr->info);
-    }
+
+Node* insert(Node* root, int key) {
+    if (!root) return new Node(key);
+    if (key<root->key) root->left=insert(root->left,key);
+    else root->right=insert(root->right,key);
+    return root;
 }
-void insert(NodeType*& tr, int a){
-    if(tr == NULL){
-        tr = new NodeType;
-        tr->left = NULL;
-        tr->right = NULL;
-        tr->info = a;
-    }else if(a > tr->info){
-        insert(tr->right, a);
-    }else{
-        insert(tr->left, a);
-    }
+
+void postorder(Node* root) {
+    if (!root) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->key<<"\n";
 }
-class tree{
-public:
-    tree(){
-        root = NULL;
-    }
-    void enqueue(int a){
-        insert(root, a);
-    }
-    void InOrderTree(queue<int>& q){
-        InOrder(root, q);
-    }
-private:
-    NodeType *root;
-};
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    queue<int> q;
-    tree t;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    Node* root=NULL;
     int n;
-    while(cin >> n){
-        if(n == EOF)
-            break;
-        t.enqueue(n);
+    while (cin>>n) {
+        root=insert(root,n);
     }
-    t.InOrderTree(q);
-    while(!q.empty()){
-        cout << q.front() << '\n';
-        q.pop();
-    }
-    return 0;
+    postorder(root);
 }
