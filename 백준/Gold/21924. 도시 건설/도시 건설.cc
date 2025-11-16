11c4512eb369270d30e53cb036b51e80881b3fa7
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-16 22:44:42 +0900

    [Gold IV] Title: 도시 건설, Time: 156 ms, Memory: 8276 KB -BaekjoonHub

---
+++ 백준/Gold/21924. 도시 건설/도시 건설.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>parent;
+
+int find(int x) {
+    if (parent[x]==x) return x;
+    else return parent[x]=find(parent[x]);
+}
+
+void unite(int a, int b) {
+    a=find(a);
+    b=find(b);
+    if (a!=b) parent[b]=a;
+}
+
+bool same(int a, int b) {
+    return find(a)==find(b);
+}
+
+class Edge {
+public:
+    int u,v,w;
+    Edge(int u, int v, int w) :u(u),v(v),w(w){}
+    bool operator<(const Edge &edge) const {
+        return w<edge.w;
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,m;
+    cin>>n>>m;
+
+    parent.resize(n+1);
+    for (int i=1; i<=n; i++) {
+        parent[i]=i;
+    }
+
+    vector<Edge>arr;
+    arr.reserve(m);
+
+    long long sum=0;
+    for (int i=0; i<m; i++) {
+        int a,b,c;
+        cin>>a>>b>>c;
+        arr.emplace_back(a,b,c);
+        sum+=c;
+    }
+
+    sort(arr.begin(),arr.end());
+
+    long long ans=0;
+    int cnt=0;
+    for (auto &i:arr) {
+        if (!same(i.u,i.v)) {
+            unite(i.u,i.v);
+            ans+=i.w;
+            cnt++;
+        }
+    }
+    cout<<(cnt==n-1?sum-ans:-1);
+}
