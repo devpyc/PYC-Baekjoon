5a1a0a2b3a97971fb4dd57e469def7b95194ea77
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-17 00:13:53 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.cc
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.cc
@@ -1,18 +1,2 @@
 #include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cin.tie(0)->sync_with_stdio(0);
-
-    int n;
-    cin>>n;
-    vector<int>arr(n);
-    for (int i=0; i<n; i++) {
-        cin>>arr[i];
-    }
-    sort(arr.begin(),arr.end());
-    arr.erase(unique(arr.begin(),arr.end()),arr.end());
-    for (int i:arr) {
-        cout<<i<<" ";
-    }
-}
+using namespace std;int main() {cin.tie(0)->sync_with_stdio(0);int n;cin>>n;vector<int>a(n);for(int i=0;i<n;i++){cin>>a[i];}sort(a.begin(),a.end());a.erase(unique(a.begin(),a.end()),a.end());for(int i:a){cout<<i<<" ";}}
