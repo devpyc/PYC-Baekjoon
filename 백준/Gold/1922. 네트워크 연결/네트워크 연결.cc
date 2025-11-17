163f24f02706ab74a9e0d59100adfe59924353e2
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-17 11:27:03 +0900

    [Gold IV] Title: 네트워크 연결, Time: 28 ms, Memory: 4460 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>parent;
+
+int find(int x) {
+    if (parent[x]==x) return x;
+    else return parent[x]=find(parent[x]);
+}
+
+void unite(int x, int y) {
+    x=find(x);
+    y=find(y);
+    if (x!=y) parent[y]=x;
+}
+
+bool same(int x, int y) {
+    return find(x)==find(y);
+}
+
+struct Edge {
+    int u,v,cost;
+    bool operator<(const Edge& edge) const{
+        return cost<edge.cost;
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,m;
+    cin>>n>>m;
+
+    parent.resize(n+1);
+    for (int i=1; i<=n; i++) {
+        parent[i]=i;
+    }
+
+    vector<Edge>arr;
+
+    while (m--) {
+        int a,b,c;
+        cin>>a>>b>>c;
+        arr.push_back({a,b,c});
+    }
+    sort(arr.begin(),arr.end());
+
+    int sum=0;
+
+    for (auto i:arr) {
+        if (!same(i.u,i.v)) {
+            unite(i.u,i.v);
+            sum+=i.cost;
+        }
+    }
+    cout<<sum;
+}
