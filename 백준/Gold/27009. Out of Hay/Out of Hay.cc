ee3b527e6230d1f099103a1bbfa616e1b4b8f4ea
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-18 12:41:15 +0900

    [Gold IV] Title: Out of Hay, Time: 4 ms, Memory: 2540 KB -BaekjoonHub

---
+++ 백준/Gold/27009. Out of Hay/Out of Hay.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>parent;
+
+int find(int x) {
+    if (parent[x]==x) return x;
+    else return parent[x]=find(parent[x]);
+}
+
+void unite(int x, int y) {
+    x=find(x);
+    y=find(y);
+    if (x!=y) parent[y]=x;
+}
+
+bool same(int x, int y) {
+    return find(x)==find(y);
+}
+
+class Edge {
+public:
+    int u,v,w;
+    Edge(int u, int v, int w) :u(u),v(v),w(w){}
+
+    bool operator<(const Edge &edge) const {
+        return w<edge.w;
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,m;
+    cin>>n>>m;
+
+    parent.resize(n+1);
+
+    for (int i=1; i<=n ;i++) {
+        parent[i]=i;
+    }
+
+    vector<Edge>arr;
+
+    while (m--) {
+        int a,b,c;
+        cin>>a>>b>>c;
+        arr.emplace_back(a,b,c);
+    }
+
+    int ans=0,MAX=0;
+    sort(arr.begin(),arr.end());
+    for (auto i:arr) {
+        if (!same(i.u,i.v)) {
+            unite(i.u,i.v);
+            ans+=i.w;
+            MAX=max(MAX,i.w);
+        }
+    }
+    cout<<MAX;
+}
