16a913898dfc76341a04e75650ed3df27581c102
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-18 13:41:24 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.cc
+++ 백준/Gold/11758. CCW/CCW.cc
@@ -1,18 +1,20 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int x1,y1,x2,y2,x3,y3;
-    cin>>x1>>y1>>x2>>y2>>x3>>y3;
-    if((x1*y2-x2*y1+x2*y3-x3*y2+x3*y1-x1*y3)/2>0){
-        cout<<"1\n";
-    }
-    else if((x1*y2-x2*y1+x2*y3-x3*y2+x3*y1-x1*y3)/2<0){
-        cout<<"-1\n";
-    }
-    else{
-        cout<<"0\n";
-    }
+int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
+    return (x2-x1)*(y3-y1)-(x3-x1)*(y2-y1);
+}
+
+struct Point {
+    int x,y;
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    Point p1,p2,p3;
+
+    cin>>p1.x>>p1.y>>p2.x>>p2.y>>p3.x>>p3.y;
+    int ans=ccw(p1.x,p1.y,p2.x,p2.y,p3.x,p3.y);
+    cout<<(ans==0?0:(ans>0?1:-1));
 }
