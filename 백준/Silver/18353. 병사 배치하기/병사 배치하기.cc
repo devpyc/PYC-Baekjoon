f589732ee5a32a9ab0e581e96dec96ec7c86e334
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-19 00:37:08 +0900

    [Silver II] Title: 병사 배치하기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.cc
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.cc
@@ -3,12 +3,12 @@ using namespace std;
 
 int lis(vector<int>&arr) {
     vector<int>lis;
-    for (int x:arr) {
-        auto it=lower_bound(lis.begin(),lis.end(),x,greater<int>());
-        if (it==lis.end()) lis.push_back(x);
-        else *it=x;
+    for (int i:arr) {
+        auto it=lower_bound(lis.begin(),lis.end(),i,greater<int>());
+        if (it==lis.end()) lis.push_back(i);
+        else *it=i;
     }
-    return static_cast<int>(lis.size());
+    return lis.size();
 }
 
 int main() {
@@ -20,5 +20,5 @@ int main() {
     for (int i=0; i<n; i++) {
         cin>>arr[i];
     }
-    cout<<n-lis(arr)<<"\n";
+    cout<<n-lis(arr);
 }
