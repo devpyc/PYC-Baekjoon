4fafe0bb20515f47936173d58f01b5a238f34609
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-19 18:12:34 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 9112 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,65 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<int,int> edge;
-static int V,E,K;
-static vector<int> mdistance;
-static vector<bool> visited;
-static vector<vector<edge>> mlist;
-static priority_queue<edge,vector<edge>,greater<edge>> q;
+vector<int>dijkstra(vector<vector<pair<int,int>>>&arr, int start) {
+    const int INF=1e9;
+    int n=arr.size();
+    vector<int>dist(n,INF);
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    dist[start]=0;
+    pq.emplace(0,start);
 
-    cin>>V>>E>>K;
-    mdistance.resize(V+1);
-    std::fill(mdistance.begin(),mdistance.end(),INT_MAX);
-    visited.reserve(V+1);
-    std::fill(visited.begin(),visited.end(),false);
-    mlist.resize(V+1);
+    while (!pq.empty()) {
+        auto [d,u]=pq.top();
+        pq.pop();
+        if (d>dist[u]) continue;
 
-    for(int i=0; i<E; i++){
-        int u,v,w;
-        cin>>u>>v>>w;
-        mlist[u].push_back(make_pair(v,w));
-    }
-
-    q.push(make_pair(0,K));
-    mdistance[K]=0;
-
-    while(!q.empty()){
-        edge current = q.top();
-        q.pop();
-        int c_v = current.second;
-        if(visited[c_v]){
-            continue;
-        }
-        visited[c_v] = true;
-
-        for(int i=0; i<mlist[c_v].size(); i++){
-            edge tmp = mlist[c_v][i];
-            int next = tmp.first;
-            int value = tmp.second;
-
-            if(mdistance[next]>mdistance[c_v]+value){
-                mdistance[next]=value+mdistance[c_v];
-                q.push(make_pair(mdistance[next],next));
+        for (auto &[v,w]:arr[u]) {
+            if (dist[u]+w<dist[v]) {
+                dist[v]=dist[u]+w;
+                pq.emplace(dist[v],v);
             }
         }
     }
-    for(int i=1; i<=V; i++){
-        if(visited[i]){
-            cout<<mdistance[i]<<"\n";
-        }
-        else{
-            cout<<"INF"<<"\n";
-        }
+    return dist;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,m,s;
+    cin>>n>>m>>s;
+
+    vector<vector<pair<int,int>>>arr(n+1);
+
+    for (int i=0; i<m; i++) {
+        int a,b,c;
+        cin>>a>>b>>c;
+        arr[a].push_back({b,c});
+    }
+    auto dist=dijkstra(arr,s);
+    for (int i=1; i<=n; i++) {
+        if (dist[i]==1e9) cout<<"INF\n";
+        else cout<<dist[i]<<"\n";
     }
 }
