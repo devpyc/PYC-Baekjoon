a254676c4e4a914e3439a372f5589c58fa10b8b3
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-23 00:08:00 +0900

    [Gold III] Title: 줄 세우기, Time: 24 ms, Memory: 4148 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -2,44 +2,38 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
     int n,m;
     cin>>n>>m;
 
     vector<vector<int>>arr(n+1);
-    vector<int>cnt(n+1,0);
-
-    for (int i = 0; i < m; i++) {
+    vector<int>cnt(n+1);
+    for (int i=0; i<m; i++) {
         int a,b;
         cin>>a>>b;
         arr[a].push_back(b);
         cnt[b]++;
     }
 
-    queue<int> q;
+    queue<int>q;
     for (int i=1; i<=n; i++) {
-        if (cnt[i]==0) {
-            q.push(i);
-        }
+        if (cnt[i]==0) q.push(i);
     }
 
     vector<int>ans;
     ans.reserve(n);
 
-    while(!q.empty()) {
+    while (!q.empty()) {
         int u=q.front();
         q.pop();
         ans.push_back(u);
 
-        for(int v:arr[u]) {
-            if(--cnt[v]==0) {
-                q.push(v);
-            }
+        for (int i:arr[u]) {
+            if (--cnt[i]==0) q.push(i);
         }
     }
-    for(int i:ans) {
-        cout<<i<<' ';
+    for (int i:ans) {
+        cout<<i<<" ";
     }
 }
