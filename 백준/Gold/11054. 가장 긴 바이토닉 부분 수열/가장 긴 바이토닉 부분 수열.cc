0aac23c8c2cfd933da947a99777dc498f3295d47
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-23 01:13:35 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -1,48 +1,30 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(0)->sync_with_stdio(0);
 
-    int N;
-    cin >> N;
-
-    vector<int> arr(N);
-    vector<int> dp_increase(N, 1);  // 증가하는 부분 수열의 길이를 저장하는 배열
-    vector<int> dp_decrease(N, 1);  // 감소하는 부분 수열의 길이를 저장하는 배열
-
-    for (int i = 0; i < N; ++i) {
-        cin >> arr[i];
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for (int i=0; i<n; i++) {
+        cin>>a[i];
     }
-
-    // 증가하는 부분 수열의 길이 계산
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < i; ++j) {
-            if (arr[i] > arr[j]) {
-                dp_increase[i] = max(dp_increase[i], dp_increase[j] + 1);
-            }
+    vector<int>lis(n,1),lds(n,1);
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<i; j++) {
+            if (a[j]<a[i]) lis[i]=max(lis[i],lis[j]+1);
         }
     }
 
-    // 감소하는 부분 수열의 길이 계산
-    for (int i = N - 1; i >= 0; --i) {
-        for (int j = N - 1; j > i; --j) {
-            if (arr[i] > arr[j]) {
-                dp_decrease[i] = max(dp_decrease[i], dp_decrease[j] + 1);
-            }
+    for (int i=n-1; i>=0; i--) {
+        for (int j=n-1; j>i; j--) {
+            if (a[j]<a[i]) lds[i]=max(lds[i],lds[j]+1);
         }
     }
-
-    int answer = 0;
-    for (int i = 0; i < N; ++i) {
-        // 가장 긴 바이토닉 부분 수열의 길이는 증가하는 부분과 감소하는 부분의 합에서 1을 뺀 값
-        answer = max(answer, dp_increase[i] + dp_decrease[i] - 1);
+    int ans=0;
+    for (int i=0; i<n; i++) {
+        ans=max(ans,lis[i]+lds[i]-1);
     }
-    cout << answer << '\n';
-
-    return 0;
+    cout<<ans;
 }
