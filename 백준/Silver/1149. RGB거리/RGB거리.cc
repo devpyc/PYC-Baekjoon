c789255bf19955dc36c06b8bd53f300ad7c78e80
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-23 11:37:51 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,37 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
     int n;
-    cin >> n;
-
-    vector<vector<int>> cost(n, vector<int>(3));
-    vector<vector<int>> dp(n, vector<int>(3, 0));
-
-    // 입력 받기
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            cin >> cost[i][j];
-        }
+    cin>>n;
+    int r,g,b;
+    cin>>r>>g>>b;
+    for (int i=2; i<=n; i++) {
+        int x,y,z;
+        cin>>x>>y>>z;
+        int nr=min(g,b)+x,ng=min(r,b)+y,nb=min(r,g)+z;
+        r=nr,g=ng,b=nb;
     }
-
-    // 다이나믹 프로그래밍
-    for (int i = 0; i < 3; ++i) {
-        dp[0][i] = cost[0][i];
-    }
-
-    for (int i = 1; i < n; ++i) {
-        dp[i][0] = cost[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
-        dp[i][1] = cost[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
-        dp[i][2] = cost[i][2] + min(dp[i - 1][0], dp[i - 1][1]);
-    }
-
-    // 최종 결과 출력
-    int result = min({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]});
-    cout << result << endl;
-
-    return 0;
+    cout<<min({r,g,b});
 }
