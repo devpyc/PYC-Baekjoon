1575dc9d61758b84d695fd21acc37ee4ddadf2d3
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-25 12:58:38 +0900

    [Gold II] Title: 문제집, Time: 28 ms, Memory: 4148 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n,m;
+    cin>>n>>m;
+
+    vector<vector<int>>arr(n+1);
+    vector<int>cnt(n+1);
+    for (int i=0; i<m; i++) {
+        int a,b;
+        cin>>a>>b;
+        arr[a].push_back(b);
+        cnt[b]++;
+    }
+
+    priority_queue<int,vector<int>,greater<>>pq;
+    for (int i=1; i<=n; i++) {
+        if (cnt[i]==0) pq.push(i);
+    }
+
+    vector<int>ans;
+    ans.reserve(n);
+
+    while (!pq.empty()) {
+        int u=pq.top();
+        pq.pop();
+        ans.push_back(u);
+
+        for (int i:arr[u]) {
+            if (--cnt[i]==0) pq.push(i);
+        }
+    }
+    for (int i:ans) {
+        cout<<i<<" ";
+    }
+}
