c2e176285b3ea5108d156230535d23ad3dc68f3a
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-26 10:45:40 +0900

    [Platinum V] Title: 카드 게임, Time: 712 ms, Memory: 33280 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.cc
+++ 백준/Platinum/16566. 카드 게임/카드 게임.cc
@@ -1,50 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> parent;
+vector<int>parent;
 
 int find(int x) {
-    if (parent[x] == x) return x;
-    return parent[x] = find(parent[x]);
+    if (parent[x]==x) return x;
+    else return parent[x]=find(parent[x]);
 }
 
-void unite(int a, int b) {
-    a = find(a);
-    b = find(b);
-    if (a != b) parent[a] = b;
+void merge(int x, int y) {
+    x=find(x);
+    y=find(y);
+    if (x!=y) parent[y]=x;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
-    int N, M, K;
-    cin >> N >> M >> K;
+    int n,m,k;
+    cin>>n>>m>>k;
 
-    vector<int> cards(M);
-    for (int i = 0; i < M; i++) {
-        cin >> cards[i];
-    }
-    sort(cards.begin(), cards.end());
-
-    parent.resize(M + 1);
-    for (int i = 0; i <= M; i++) {
-        parent[i] = i;
+    vector<int>arr(m);
+    for (int i=0; i<m; i++) {
+        cin>>arr[i];
     }
 
-    for (int i = 0; i < K; i++) {
-        int cheolsu;
-        cin >> cheolsu;
+    sort(arr.begin(),arr.end());
 
-        int idx = upper_bound(cards.begin(), cards.end(), cheolsu) - cards.begin();
-        idx = find(idx);
-
-        cout << cards[idx] << '\n';
-
-        if (idx < M - 1) {
-            unite(idx, idx + 1);
-        }
+    parent.resize(m+1);
+    for (int i=1; i<=m; i++) {
+        parent[i]=i;
     }
 
-    return 0;
+    while (k--) {
+        int x;
+        cin>>x;
+        auto i=upper_bound(arr.begin(),arr.end(),x)-arr.begin();
+        i=find(i);
+        cout<<arr[i]<<"\n";
+        parent[i]=find(i+1);
+    }
 }
