a2b0ad7c0d48569b39baa2a7154ff2cd3a322f3d
devpyc <136340169+devpyc@users.noreply.github.com>
2025-11-26 11:03:48 +0900

    [Platinum V] Title: 행성 터널, Time: 88 ms, Memory: 7500 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.cc
+++ 백준/Platinum/2887. 행성 터널/행성 터널.cc
@@ -1,73 +1,76 @@
 #include <bits/stdc++.h>
-#define endl "\n"
 using namespace std;
 
-struct Edge {
-    int start;
-    int end;
-    int cost;
-};
-
-vector<int> parent;
+vector<int>parent;
 
 int find(int x) {
-    if (parent[x] == x) {
-        return x;
-    }
-    return parent[x] = find(parent[x]);
+    if (parent[x]==x) return x;
+    else return parent[x]=find(parent[x]);
 }
 
-void unionSet(int x, int y) {
-    int rootX = find(x);
-    int rootY = find(y);
-    if (rootX != rootY) {
-        parent[rootY] = rootX;
-    }
+void merge(int x, int y) {
+    x=find(x);
+    y=find(y);
+    if (x!=y) parent[y]=x;
 }
 
-bool compareEdge(const Edge& a, const Edge& b) {
-    return a.cost < b.cost;
+bool same(int x, int y) {
+    return find(x)==find(y);
 }
+
+struct Point {
+    int idx,x,y,z;
+};
+
+struct Edge {
+    int u,v,w;
+    bool operator<(const Edge &edge) const {
+        return w<edge.w;
+    }
+};
 
 int main() {
-    int N;
-    cin >> N;
+    cin.tie(0)->sync_with_stdio(0);
 
-    vector<pair<int, int>> x(N);
-    vector<pair<int, int>> y(N);
-    vector<pair<int, int>> z(N);
+    int n;
+    cin>>n;
 
-    for (int i = 0; i < N; i++) {
-        cin >> x[i].first >> y[i].first >> z[i].first;
-        x[i].second = y[i].second = z[i].second = i;
+    vector<Point>arr(n);
+    for (int i=0; i<n; i++) {
+        cin>>arr[i].x>>arr[i].y>>arr[i].z;
+        arr[i].idx=i;
     }
 
-    sort(x.begin(), x.end());
-    sort(y.begin(), y.end());
-    sort(z.begin(), z.end());
+    vector<Edge>edge;
+    edge.reserve((n-1)*3);
 
-    vector<Edge> edges;
-    for (int i = 0; i < N - 1; i++) {
-        edges.push_back({x[i].second, x[i + 1].second, abs(x[i].first - x[i + 1].first)});
-        edges.push_back({y[i].second, y[i + 1].second, abs(y[i].first - y[i + 1].first)});
-        edges.push_back({z[i].second, z[i + 1].second, abs(z[i].first - z[i + 1].first)});
-    }
+    for (int i=0; i<3; i++) {
+        if (i==0) sort(arr.begin(),arr.end(),[](auto &a, auto &b){return a.x<b.x;});
+        else if (i==1) sort(arr.begin(),arr.end(),[](auto &a, auto &b){return a.y<b.y;});
+        else sort(arr.begin(),arr.end(),[](auto &a, auto &b){return a.z<b.z;});
 
-    sort(edges.begin(), edges.end(), compareEdge);
-
-    parent.resize(N);
-    for (int i = 0; i < N; i++) {
-        parent[i] = i;
-    }
-
-    int totalCost = 0;
-    for (const auto& edge : edges) {
-        if (find(edge.start) != find(edge.end)) {
-            totalCost += edge.cost;
-            unionSet(edge.start, edge.end);
+        for (int j=0; j<n-1; j++) {
+            int dx=abs(arr[j].x-arr[j+1].x),dy=abs(arr[j].y-arr[j+1].y),dz=abs(arr[j].z-arr[j+1].z);
+            int w=min({dx,dy,dz});
+            edge.push_back({arr[j].idx,arr[j+1].idx,w});
         }
     }
-    cout << totalCost << endl;
+    sort(edge.begin(),edge.end());
 
-    return 0;
+    parent.resize(n);
+    for (int i=0; i<n; i++) {
+        parent[i]=i;
+    }
+
+    long long ans=0;
+    int cnt=0;
+
+    for (auto &i:edge) {
+        if (!same(i.u,i.v)) {
+            merge(i.u,i.v);
+            ans+=i.w;
+            if (++cnt==n-1) break;
+        }
+    }
+    cout<<ans;
 }
