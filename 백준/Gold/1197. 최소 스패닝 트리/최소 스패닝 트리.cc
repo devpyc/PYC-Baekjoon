bbba9191e7bcb608ef74a28ae07274055d6e55bb
devpyc <136340169+devpyc@users.noreply.github.com>
2025-12-01 11:18:37 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -3,26 +3,25 @@ using namespace std;
 
 vector<int>parent;
 
-int Find(int x) {
+int find(int x) {
     if (parent[x]==x) return x;
-    else return parent[x]=Find(parent[x]);
+    else return parent[x]=find(parent[x]);
 }
 
-void Union(int x, int y) {
-    x=Find(x);
-    y=Find(y);
+void merge(int x, int y) {
+    x=find(x);
+    y=find(y);
     if (x!=y) parent[y]=x;
 }
 
 bool same(int x, int y) {
-    return Find(x)==Find(y);
+    return find(x)==find(y);
 }
 
 class Edge {
 public:
     int u,v,w;
-    Edge(int u, int v, int w) :u(u),w(w),v(v){}
-
+    Edge(int u, int v, int w):u(u),v(v),w(w){}
     bool operator<(const Edge &edge) const {
         return w<edge.w;
     }
@@ -38,22 +37,20 @@ int main() {
     for (int i=1; i<=n; i++) {
         parent[i]=i;
     }
-
     vector<Edge>arr;
 
     while (m--) {
-        int u,v,w;
-        cin>>u>>v>>w;
-        arr.emplace_back(u,v,w);
+        int a,b,c;
+        cin>>a>>b>>c;
+        arr.emplace_back(a,b,c);
     }
 
     sort(arr.begin(),arr.end());
 
-    int ans=0;
-
-    for (auto i:arr) {
+    long long ans=0;
+    for (auto &i: arr) {
         if (!same(i.u,i.v)) {
-            Union(i.u,i.v);
+            merge(i.u,i.v);
             ans+=i.w;
         }
     }
