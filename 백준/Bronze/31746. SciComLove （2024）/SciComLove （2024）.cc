14179bee9bb0a0fd6a0877c046b83dd865910eb4
devpyc <136340169+devpyc@users.noreply.github.com>
2025-12-02 10:05:21 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -1,15 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n;
-    string s="SciComLove";
     cin>>n;
-
-    if(n%2==1){
-        reverse(s.begin(),s.end());
-        cout<<s;
-    }else{
-        cout<<s;
-    }
+    string s="SciComLove";
+    if (n%2==1) reverse(s.begin(),s.end());
+    cout<<s;
 }
