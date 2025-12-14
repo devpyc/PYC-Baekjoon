227c04dff00b3c313af08676f15112186087a248
devpyc <136340169+devpyc@users.noreply.github.com>
2025-12-14 12:56:43 +0900

    [Bronze V] Title: 이중 반복문, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    long long a;
+    cin>>a;
+    cout<<(a*a>1e8?"Time limit exceeded":"Accepted");
+}
