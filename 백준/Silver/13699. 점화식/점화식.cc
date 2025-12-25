a3e7d47a867f4b8af100f51893b8c36f811b2933
devpyc <136340169+devpyc@users.noreply.github.com>
2025-12-25 23:05:18 +0900

    [Silver IV] Title: 점화식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13699. 점화식/점화식.cc
+++ 백준/Silver/13699. 점화식/점화식.cc
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
 
-    long long dp[36]={1};
     int n;
     cin>>n;
 
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=i; j++) {
-            dp[i]+=dp[j-1]*dp[i-j];
-        }
+    unsigned long long dp[36]={0};
+    dp[0]=1;
+    for (int i=1; i<=n; i++) {
+       for (int j=0; j<i; j++) {
+           dp[i]+=dp[j]*dp[i-j-1];
+       }
     }
     cout<<dp[n];
 }
