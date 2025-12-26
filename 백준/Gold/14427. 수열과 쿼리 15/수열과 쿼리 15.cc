bd507e4e200205aca02ce9a52502b9438e43ca13
devpyc <136340169+devpyc@users.noreply.github.com>
2025-12-26 17:49:33 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 112 ms, Memory: 9148 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.cc
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.cc
@@ -1,102 +1,42 @@
 #include <bits/stdc++.h>
-#define int long long
-#define endl "\n"
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
-const int INF = 1e9+1;
+typedef tree<pair<long long,int>,null_type,less<pair<long long,int>>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
 
-struct SegmentTree {
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
     int n;
-    vector<int> tree, idx;
+    cin>>n;
 
-    SegmentTree(const vector<int>& data) {
-        n = data.size();
-        tree.resize(4 * n);
-        idx.resize(4 * n);
-        build(data, 1, 0, n - 1);
+    vector<long long>arr(n+1);
+    ordered_set s;
+
+    for (int i=1; i<=n; i++) {
+        cin>>arr[i];
+        s.insert({arr[i],i});
     }
 
-    void build(const vector<int>& data, int node, int start, int end) {
-        if (start == end) {
-            tree[node] = data[start];
-            idx[node] = start;
-        } else {
-            int mid = (start + end) / 2;
-            build(data, node * 2, start, mid);
-            build(data, node * 2 + 1, mid + 1, end);
-            if (tree[node * 2] <= tree[node * 2 + 1]) {
-                tree[node] = tree[node * 2];
-                idx[node] = idx[node * 2];
-            } else {
-                tree[node] = tree[node * 2 + 1];
-                idx[node] = idx[node * 2 + 1];
-            }
+    int m;
+    cin>>m;
+
+    while (m--) {
+        int tt;
+        cin>>tt;
+
+        if (tt==1) {
+            int a;
+            long long b;
+            cin>>a>>b;
+
+            s.erase({arr[a],a});
+
+            arr[a]=b;
+            s.insert({arr[a],a});
+        }else {
+            cout<<s.begin()->second<<"\n";
         }
     }
-
-    void update(int pos, int value, int node, int start, int end) {
-        if (start == end) {
-            tree[node] = value;
-            idx[node] = start;
-        } else {
-            int mid = (start + end) / 2;
-            if (pos <= mid) {
-                update(pos, value, node * 2, start, mid);
-            } else {
-                update(pos, value, node * 2 + 1, mid + 1, end);
-            }
-            if (tree[node * 2] <= tree[node * 2 + 1]) {
-                tree[node] = tree[node * 2];
-                idx[node] = idx[node * 2];
-            } else {
-                tree[node] = tree[node * 2 + 1];
-                idx[node] = idx[node * 2 + 1];
-            }
-        }
-    }
-
-    pair<int, int> query(int L, int R, int node, int start, int end) {
-        if (R < start || end < L) return {INF, -1};
-        if (L <= start && end <= R) return {tree[node], idx[node]};
-        int mid = (start + end) / 2;
-        pair<int, int> left = query(L, R, node * 2, start, mid);
-        pair<int, int> right = query(L, R, node * 2 + 1, mid + 1, end);
-        if (left.first <= right.first) return left;
-        return right;
-    }
-
-    void update(int pos, int value) {
-        update(pos, value, 1, 0, n - 1);
-    }
-
-    int query() {
-        return idx[1] + 1;
-    }
-};
-
-int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int N, M;
-    cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> A[i];
-    }
-    SegmentTree segTree(A);
-    cin >> M;
-    for (int i = 0; i < M; ++i) {
-        int type;
-        cin >> type;
-        if (type == 1) {
-            int index, value;
-            cin >> index >> value;
-            segTree.update(index - 1, value);
-        } else if (type == 2) {
-            cout << segTree.query() << endl;
-        }
-    }
-
-    return 0;
 }
