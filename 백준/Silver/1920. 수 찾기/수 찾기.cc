ed8feda9f69df7a81df1fe72c47095421c858164
devpyc <136340169+devpyc@users.noreply.github.com>
2025-12-29 11:27:31 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
@@ -10,12 +9,31 @@ int main() {
     for (int i=0; i<n; i++) {
         cin>>arr[i];
     }
+
     sort(arr,arr+n);
+
     int m;
     cin>>m;
     while (m--) {
-        int x;
-        cin>>x;
-        cout<<binary_search(arr,arr+n,x)<<"\n";
+        int target;
+        int left=0,right=n-1;
+        cin>>target;
+        bool check=false;
+        while(left<=right){
+            int mid=(left+right)/2;
+        
+            if (arr[mid]==target) {
+                check=true;
+            }
+        
+            if (arr[mid]<target) {
+                left=mid+1;
+            }
+            else if (arr[mid]>=target) {
+                right=mid-1;
+            }
+        }
+        if (check) cout<<1<<"\n";
+        else cout<<0<<"\n";
     }
 }
