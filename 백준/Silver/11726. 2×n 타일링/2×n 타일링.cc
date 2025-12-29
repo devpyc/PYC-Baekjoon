8d9027d171c4597dee787e86b5edcf4965510a45
devpyc <136340169+devpyc@users.noreply.github.com>
2025-12-29 12:29:29 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
-#define MOD 10007
 using namespace std;
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
+
 
     int n;
     cin>>n;
-    int dp[n+1];
-
-    dp[1]=1,dp[2]=2;
-    for(int i=3; i<n+1; ++i) {
-        dp[i]=(dp[i-1]+dp[i-2])%MOD;
+    int dp[1001];
+    dp[1]=1;
+    dp[2]=2;
+    for (int i=3; i<=n; i++) {
+        dp[i]=(dp[i-2]+dp[i-1])%10007;
     }
     cout<<dp[n];
 }
